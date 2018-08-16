9fa9332a1d6b06953ec81310766a81764d436893
jas <jas@localhost.localdomain>
2018-08-16 12:01:47 -0700

    Update

--- conductor/Conductor.py
+++ conductor/Conductor.py
@@ -1,6 +1,5 @@
 import base64
 import binascii
-from decimal import Decimal
 from random import randrange
 
 PRIME = 257
@@ -13,24 +12,24 @@ class Conductor(object):
         self.ENTROPY_CUTOFF = ENTROPY_CUTOFF
         self.FERMAT_CUTOFF = FERMAT_CUTOFF
 
-        self.element = element                                # 8. Like A Disc With A Mass
-        self.ring = pow(ring, element, self.FERMAT_CUTOFF)    # 11. Like A Binocular Focus Through A Medium
-        self.barn = pow(barn, element, self.FERMAT_CUTOFF)    # 12. The Nuclear Barn
+        self.element = element      # 8. Like A Radius Without A Thickness
+        self.ring = ring            # 11. Like A Binocular Focus Through A Medium
+        self.barn = barn            # 12. The Nuclear Barn
 
     # Charge Results In A 5th Column, What A Barn Is Defined As Containing While It Exists
     # The Audit Says That The Ampere Is A Unit Of Charge With Specific Containment.
     # A Charge With A Containment Is A Labor But Not A Work
     def charge(self, voltpere):
-        return pow(self.barn, voltpere, self.ring)
+        return pow(self.barn, voltpere, self.FERMAT_CUTOFF)
 
     # Induce (A Labor) Results In A Henry, A 5th Column With Dimensional Containment
     # Hard Definition Of ProofOfWork
     def induce(self, ampere):
-        return pow(ampere, self.barn, self.ring)
+        return pow(ampere, self.barn, self.FERMAT_CUTOFF)
 
     # Ringing The Henry Through The Barn Refracts A Maxwell
     def refract(self, henry):
-        return pow(self.ring, henry, self.barn)
+        return pow(self.ring, henry, self.FERMAT_CUTOFF)
 
     # Refracting The Maxwell Results In A Fermat
     def prime(self, maxwell):
@@ -40,9 +39,7 @@ class Conductor(object):
         stringUnicode = string.encode('utf-8')
         stringHex = binascii.hexlify(stringUnicode)
         stringInt = int(stringHex, 16)
-        hashNumber = pow(stringInt, self.element, self.ring)
-#        hashCode = base64.b64encode(bytes(str(Decimal(hashNumber) ** (Decimal(1) / Decimal(self.identity))), encoding = 'ascii')) 
-#        return hashCode
+        hashNumber = pow(self.element, stringInt, pow(self.ring, self.barn))
         return hashNumber
 
 
