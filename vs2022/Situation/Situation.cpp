291a917bdc7211885b7a392e87e8680c18da1a53
maria <maria@twee.se>
2022-11-19 12:49:23 -0800

    Patent Confirmation

--- vs2022/Situation/Situation.cpp
+++ vs2022/Situation/Situation.cpp
@@ -266,6 +266,7 @@ namespace Dysnomia {
 				case 'A': // Patent Arrow Pushing Mechanism
 					R->RemoveLast(); // Patent Phosphonium
 					P = C3PO(R, NZ, P); // Trademark Phosphone Third Paradox
+					// Patent Confirmation
 					NR = R->AddAfter(NZ, '5'); // Patent Phosphinium
 					NR = R->AddAfter(NR, '6'); // Trademark THPC
 					return Deprotonate(NR, R, E, P); // Patent Tris(hydroxymethyl)phosphine 
