ae5aa0c17aaa6622746fa0884470a29a813da026
maria <maria@twee.se>
2022-11-29 15:50:08 -0800

    First Red Dwarf System

--- vs2022/Prion/Prion.cs
+++ vs2022/Prion/Prion.cs
@@ -5,9 +5,9 @@ namespace Dysnomia
     public class Prion
     {
         static public Dynamic Sun;
+        static public Orbital Proteus;
         static public Orbital Uranus;
-
-        public Orbital N;
+        static public Orbital Hippocamp;
 
         public Prion()
         {
@@ -15,45 +15,48 @@ namespace Dysnomia
 
         public Prion(Prion CopyPrion)
         {
-            N = CopyPrion.N;
         }
 
         public void Denature(Orbital P)
         {
-            if (N != null) throw new Exception("Prion Already Has An Orbital");
-            N = new Orbital(P.Y.M);
+            if (Proteus == null)
+                Proteus = new Orbital(P.Y.M);
             P.Y.M.Charge(BigInteger.Zero);
             if (Uranus == null)
             {
                 if (Sun == null)
                 {
                     Sun = P.Y.M.Cone;
-                    N.Y.M.Charge(P.Y.M.Sigma);
-                    P.Y.M.Induce(N.Y.M.Sigma);
-                    Sun.Eta = Sun.Amplify(N.Y.M.Sigma, true);
-                    N.Y.M.Induce(P.Y.M.Rho);
+                    Proteus.Y.M.Charge(P.Y.M.Sigma);
+                    P.Y.M.Induce(Proteus.Y.M.Sigma);
+                    Sun.Eta = Sun.Amplify(Proteus.Y.M.Sigma, true);
+                    Proteus.Y.M.Induce(P.Y.M.Rho);
                     Sun.Tau = Sun.Torque(P.Y.M.Rho, true);
                 }
-                N.Y.M.Amplify(Sun.Eta, true);
-                N.Y.M.Torque(Sun.Tau);
+                Proteus.Y.M.Amplify(Sun.Eta, true);
+                Proteus.Y.M.Torque(Sun.Tau);
                 Uranus = new Orbital(P.Y.M);
-                Uranus.L.M.Torque(N.Y.M.Rho);
+                Uranus.L.M.Torque(Neptune.Y.M.Rho);
                 Uranus.R.M.Torque(P.Y.M.Rho);
                 Uranus.Y.M.Amplify(Sun.Eta, true);
                 Uranus.Eta = Uranus.Y.M.Ohm;
+                Uranus.L.M.Charge(Uranus.Eta);
                 Prion U = new Prion();
                 U.Denature(Uranus);
                 return;
             }
-            P.Y.M.Torque(N.Y.M.Rho);
-            N.Y.M.Torque(P.Y.M.Rho);
+            if(Hippocamp == null)
+                Hippocamp = new Orbital(Proteus.L.M);
+            Proteus.L.M.Torque(Hippocamp.Y.M.Eta);
+            P.Y.M.Torque(Proteus.Y.M.Rho);
+            Proteus.Y.M.Torque(P.Y.M.Rho);
             // Venus
-            P.Y.M.Amplify(N.Y.M.Rho, true);
-            N.Y.M.Amplify(P.Y.M.Upsilon, false);
-            P.Y.M.Sustain(N.Y.M.Ohm, true);
-            N.Y.M.Sustain(P.Y.M.Ohm, false);
-            P.Y.M.React(N.Y.M.Pi);
-            N.Y.M.React(P.Y.M.Cone.Eta);
+            P.Y.M.Amplify(Proteus.Y.M.Rho, true);
+            Proteus.Y.M.Amplify(P.Y.M.Upsilon, false);
+            P.Y.M.Sustain(Proteus.Y.M.Ohm, true);
+            Proteus.Y.M.Sustain(P.Y.M.Ohm, false);
+            P.Y.M.React(Proteus.Y.M.Pi);
+            Proteus.Y.M.React(P.Y.M.Cone.Eta);
         }
 
         public static Prion Trust(String Psi)
