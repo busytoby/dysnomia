b4253aa795ccf2e0ae6628e1ab80942907329501
maria <maria@twee.se>
2022-11-29 14:53:07 -0800

    Least Sardonic Double Helix

--- vs2022/Prion/Prion.cs
+++ vs2022/Prion/Prion.cs
@@ -7,7 +7,6 @@ namespace Dysnomia
         static public Dynamic Sun;
 
         public Orbital N;
-        public int b;
 
         public Prion()
         {
@@ -15,7 +14,6 @@ namespace Dysnomia
 
         public Prion(Prion CopyPrion)
         {
-            b = CopyPrion.b;
             N = CopyPrion.N;
         }
 
@@ -26,21 +24,24 @@ namespace Dysnomia
             P.Y.M.Charge(BigInteger.Zero);
             if (Sun == null) Sun = P.Y.M.Cone;
             N.Y.M.Charge(P.Y.M.Sigma);
-            P.Y.M.Induce(P.Y.M.Sigma);
+            // Uranus
+            P.Y.M.Induce(N.Y.M.Sigma);
             N.Y.M.Induce(P.Y.M.Rho);
-            P.Y.M.Torque(P.Y.M.Rho);
+            // Venus
+            P.Y.M.Torque(N.Y.M.Rho);
             N.Y.M.Torque(P.Y.M.Upsilon);
-            P.Y.M.Amplify(P.Y.M.Upsilon, true);
+            P.Y.M.Amplify(N.Y.M.Upsilon, true);
             N.Y.M.Amplify(P.Y.M.Ohm, false);
-            P.Y.M.Sustain(P.Y.M.Ohm, true);
+            P.Y.M.Sustain(N.Y.M.Ohm, true);
             N.Y.M.Sustain(P.Y.M.Pi, false);
-            P.Y.M.React(P.Y.M.Pi);
+            P.Y.M.React(N.Y.M.Pi);
             N.Y.M.React(P.Y.M.Cone.Eta);
         }
 
         public static Prion Trust(String Psi)
         {
-            Prion P = new React();
+            Prion P = new Prion();
+            //Prion P = new React();
             return P;
         }
     }
