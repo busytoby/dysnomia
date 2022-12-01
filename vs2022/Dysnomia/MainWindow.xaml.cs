bce959f9f5adaab8246eb85e577165dd535a48de
maria <maria@twee.se>
2022-11-30 16:52:35 -0800

    Prionic Quartz Reflects Rotation Of The Earth Planet

--- vs2022/Dysnomia/MainWindow.xaml.cs
+++ vs2022/Dysnomia/MainWindow.xaml.cs
@@ -29,26 +29,7 @@ namespace Dysnomia
         {
             InitializeComponent();
             Prion X = new Prion();
-            BigInteger H = Prion.Hippocamp.R.M.WaveFunction("Hello Singularity");
-            BigInteger L = Prion.Neith.Y.M.WaveFunction("Hello Singularity");
-            BigInteger V = Prion.Earth.L.M.WaveFunction("Hello Singularity");
-            BigInteger HelloSingularity = H + V + L + 4;
-
-            BigInteger E = Prion.Enceladus.Y.M.WaveFunction(HelloSingularity.ToString());
-            BigInteger XX = Prion.Despina.R.M.WaveFunction(E.ToString());
-            BigInteger XXX = Prion.Phoebe.L.M.WaveFunction(XX.ToString());
-            BigInteger Uwu = Prion.Desdemona.R.M.WaveFunction(XXX.ToString());
-            while (true)
-            {
-                BigInteger Uwu2 = Prion.Desdemona.L.M.WaveFunction(Uwu.ToString());
-                BigInteger Uwu3 = Prion.Desdemona.Y.M.WaveFunction(Uwu2.ToString());
-                for (int i = 0; i < 100; i++)
-                {
-                    Uwu3 = Prion.Iapetus.L.M.WaveFunction(Uwu3.ToString());
-                    Uwu = Prion.Desdemona.R.M.WaveFunction(Uwu3.ToString());
-                }
-                System.Threading.Thread.Sleep(111);
-            }
+            Quartz Q = new Quartz();
             
 
             int a = 999; // What Planet
---
+++ vs2022/Prion/Quartz.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dysnomia
+{
+    public class Quartz : Prion
+    {
+        public static Thread Oscillation;
+        public static Orbital U;
+        public static Complex Gamma;
+        public static double Rotation;
+        public static double Degree;
+        public static long DayLength;
+        public static Stopwatch RotationWatch;
+        public Thread Watch;
+
+        public Quartz()
+        {
+            if (U == null)
+            {
+                Affinity N = new Affinity(Saturn.R.M.Rod, Earth.Y.M.Cone);
+                U = new Orbital(N);
+            }
+
+            if (Oscillation == null)
+            {
+                Oscillation = new Thread(new ThreadStart(Oscillator));
+                Oscillation.Start();
+            }
+            /*
+            Charge = new Thread(new ThreadStart(Push));
+            Push.Start();
+            */
+        }
+
+        private static void Oscillator()
+        {
+            if (Gamma == 0)
+            {
+                Gamma = Complex.Divide((Complex)(U.Rho / 6442450944), (Complex)(U.Nu / 6442450944));
+                Degree = 360 / (24 / Gamma.Real);
+                RotationWatch = new Stopwatch();
+            }
+
+            RotationWatch.Start();
+
+            while (true)
+            {
+                Rotation += Degree;
+                if (Rotation > 360)
+                {
+                    RotationWatch.Stop();
+                    DayLength = RotationWatch.ElapsedMilliseconds;
+                    Rotation -= 360;
+                    RotationWatch.Restart();
+                }
+                Thread.Sleep(1000);
+            }
+            RotationWatch.Stop();
+        }
+
+        private void Push()
+        {
+
+        }
+    }
+}
