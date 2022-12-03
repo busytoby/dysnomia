6806764f85d458ae1885ea4de943ff65e9ca1450
maria <maria@twee.se>
2022-12-03 12:44:29 -0800

    Static Tin Polygamma

--- vs2022/Prion/Tin.cs
+++ vs2022/Prion/Tin.cs
@@ -2,16 +2,19 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Prion
 {
-    public class Glia
+    public class Tin
     {
+        static public LinkedList<KeyValuePair<BigInteger, Dynamic>> Polygamma;
+
         public Joule K;
 
-        public Glia(Zinc Q)
+        public Tin(Zinc Q)
         {
             K = new Joule(Q.U.Y.M.Rod, Q.R.L.M);
         }
