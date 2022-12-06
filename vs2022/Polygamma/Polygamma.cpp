cd5edf0761e24b96881031d4bf972548322e14af
maria <maria@twee.se>
2022-12-06 12:36:37 -0800

    First Monera Genetic Differentiations

--- vs2022/Polygamma/Polygamma.cpp
+++ vs2022/Polygamma/Polygamma.cpp
@@ -64,16 +64,25 @@ namespace Dysnomia {
     }
 
     void Polygamma::Add(Quaternion^ S) {
-        Ion^ I = gcnew Ion(S->Nu);
-        Ion^ R = gcnew Ion(S->Phi);
+        Ion^ I = gcnew Ion(S->Phi);
+        Ion^ R = gcnew Ion(S->Nu);
         Ion^ N = gcnew Ion(S->Sigma);
 
         Quaternion^ V = gcnew Quaternion();
         V->Gamma = gcnew Affinity(T->Last->Value.Value->Gamma->Rod, I->M->Cone);
-        V->Nu = gcnew Affinity(T->Last->Value.Value->Rho->Rod, S->Nu->Cone);
-        V->Phi = gcnew Affinity(T->Last->Value.Value->Gamma->Rod, S->Phi->Cone);
-        V->Rho = gcnew Affinity(S->Rho->Rod, T->Last->Value.Value->Rho->Cone);
-        V->Sigma = gcnew Affinity(R->M->Rod, S->Sigma->Cone);
+        V->Nu = gcnew Affinity(T->Last->Value.Value->Rho->Rod, S->Phi->Cone);
+        if (S->Gamma->Rod->Signal > T->Last->Value.Value->Gamma->Rod->Signal)
+            V->Phi = gcnew Affinity(S->Gamma->Rod, T->Last->Value.Value->Nu->Cone);
+        else
+            V->Phi = gcnew Affinity(T->Last->Value.Value->Gamma->Rod, S->Nu->Cone);
+        if(S->Rho->Rod->Foundation > T->Last->Value.Value->Rho->Cone->Foundation)
+            V->Rho = gcnew Affinity(S->Rho->Rod, T->Last->Value.Value->Rho->Cone);
+        else
+            V->Rho = gcnew Affinity(T->Last->Value.Value->Rho->Rod, S->Rho->Cone);
+        if(S->Phi->Cone->Barn > R->M->Rod->Barn)
+            V->Sigma = gcnew Affinity(R->M->Rod, S->Sigma->Cone);
+        else
+            V->Sigma = gcnew Affinity(S->Sigma->Rod, R->M->Cone);
 
         V->Epsilon = Math::ModPow(T->Last->Value.Key, T->Last->Value.Value->Nu->Cone->Manifold, S->Sigma->Rod->Barn);
 
