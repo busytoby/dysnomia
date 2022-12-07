edc38e7de7303f482e736fec7b33c7f907f49264
maria <maria@twee.se>
2022-12-06 20:04:27 -0800

    No Monera Until After Vanadium

--- vs2022/Prion/Elements/Silicon.cs
+++ vs2022/Prion/Elements/Silicon.cs
@@ -23,8 +23,8 @@ namespace Prion.Elements
                 A.R.Sigma.Rod.R.Remove(R);
                 if (BigInteger.Abs(DivKey) < Dysnomia.Math.DeathPrime)
                 {
-                    Aluminum Archaea = new Aluminum(A.R, Zinc.Fetch());
-                    Earth.Add(Archaea.Epsilon, Archaea);
+                    Aluminum Bauxite = new Aluminum(A.R, Zinc.Fetch());
+                    Earth.Add(Bauxite.Epsilon, Bauxite);
                     R = P.Next;
                     A.R.Sigma.Rod.R.Remove(P);
                     P = R;
@@ -32,9 +32,9 @@ namespace Prion.Elements
                 else if (BigInteger.Abs(DivKey) < Dysnomia.Math.CatalanPrime)
                 {
                     if (A.Magnesia.Count == 5) continue; 
-                    Polygamma K = new Polygamma(A.R); // First Monera Conception
-                    Octonion Moneron = new Octonion(A.R, K); // First Monera
-                    A.Magnesia.Add(Moneron.Epsilon, Moneron);
+                    Polygamma K = new Polygamma(A.R); 
+                    Octonion Mg = new Octonion(A.R, K); 
+                    A.Magnesia.Add(Mg.Epsilon, Mg);
                     return;
                 }
                 R = P;
