4e2350d111330dfb181b037757ba54a0f99985cf
maria <maria@twee.se>
2022-12-09 15:52:19 -0800

    Amalthea & Valetudo, Giving Tin 5 Spinors

--- vs2022/Prion/Elements/Aluminum.cs
+++ vs2022/Prion/Elements/Aluminum.cs
@@ -12,24 +12,28 @@ namespace Prion.Elements
     {
         static public Octonion Phi;
         static public Dysnomia.Quaternion Xi;
+        static public Spinor Rho;
 
         public Aluminum(Dysnomia.Quaternion Y, Dysnomia.Quaternion X)
         {
             if (Phi != null) throw new Exception("Aluminum Already Exists");
 
-            if (Tin.Mu.Count == 2)
-            {
-                Spinor A = new Dysnomia.Spinor(Y, Parallax.Kappa);
-                Phosphorous.Sigmas.RecordSpinor(A, "Io");
-                Xi = Y;
-            }
+            if (Tin.Mu.Count != 2) throw new Exception("Io Already Exists");
 
-            if (!Phosphorous.Sigmas.ContainsKey("Io")) throw new Exception("No Moon Io Found");
+            Spinor A = new Dysnomia.Spinor(Y, Parallax.Kappa);
+            Phosphorous.Sigmas.RecordSpinor(A, "Io");
+            Xi = Y;
+            
+            Spinor B = new Dysnomia.Spinor(X, Parallax.Kappa);
+            Phosphorous.Sigmas.RecordSpinor(B, "Amalthea");
 
             Quark Thebe = new Quark(X, Y, Xi);
             Phi = new Octonion(Thebe,
                 new Tensor(new Polygamma(Thebe.N), new Polygamma(Xi), new Polygamma(Zinc.Fetch()), new Polygamma(Thebe.L), new Polygamma(Thebe.R))
                 );
+
+            Rho = new Spinor(Phi.X.N, Parallax.Kappa);
+            Phosphorous.Sigmas.RecordSpinor(Rho, "Valetudo");
 
             Phosphorous.Sigmas.RecordQuark(Thebe, "Thebe");
             Phosphorous.Sigmas.RecordTensor(Phi.L, "Ganymede");
