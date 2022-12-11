c0dec06ddb7382d04d36ad9f61bd6877f29df221
maria <maria@twee.se>
2022-12-10 17:53:59 -0800

    Cleanup

--- vs2022/Prion/Elements/Chlorine.cs
+++ vs2022/Prion/Elements/Chlorine.cs
@@ -10,19 +10,6 @@ namespace Prion.Elements
 {
     public class Chlorine<T> : Shift<T>
     {
-        public Chlorine(LinkedList<KeyValuePair<BigInteger, T>> In) : base(In)
-        { }
-            //Shift<T> t = new Shift<T>(In);
-
-            /*
-            LinkedListNode<KeyValuePair<BigInteger, T>> INode = In.First;
-
-            for (int j = 0; j < In.Count; j++)
-            {
-                this.Add(INode.Value.Value);
-                INode = INode.Next;
-            }
-            */
-        //}
+        public Chlorine(LinkedList<KeyValuePair<BigInteger, T>> In) : base(In) { }
     }
 }
