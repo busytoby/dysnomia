6ff0c75bd8eeb3907d9999eed7e31e59741e4476
maria <maria@twee.se>
2022-12-15 15:09:58 -0800

    Bundle Stubbing

--- vs2022/Quark/Bundle.h
+++ vs2022/Quark/Bundle.h
@@ -1,1 +1,18 @@
 #pragma once
+#include "Quark.h"
+
+using namespace System;
+using namespace System::Numerics;
+using namespace System::Collections::Generic;
+
+namespace Dysnomia {
+	public ref class Bundle {
+		BigInteger Epsilon;
+
+		Tuple<Quark^, Quark^>^ Gamma;
+		Tuple<Quark^, Quark^>^ Nu;
+		Tuple<Quark^, Quark^>^ Phi;
+		Tuple<Quark^, Quark^>^ Rho;
+		Tuple<Quark^, Quark^>^ Sigma;
+	};
+}
