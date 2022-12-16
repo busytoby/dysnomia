a3015efcb42ea130f16697e159c7f208db32818c
maria <maria@twee.se>
2022-12-15 17:27:44 -0800

    Chill

--- vs2022/Octonion/Bundle.cpp
+++ vs2022/Octonion/Bundle.cpp
@@ -56,13 +56,13 @@ namespace Dysnomia {
 			gcnew Quark(gcnew Quaternion(), gcnew Quaternion(), Phi2));
 
 		Quaternion^ Rho1 = gcnew Quaternion();
-		Rho1->Gamma = gcnew Affinity(R->Gamma->Item1->Rod, R->Gamma->Item1->Cone);
+		Rho1->Gamma = gcnew Affinity(gcnew Dynamic(), R->Gamma->Item1->Cone);
 		Rho1->Nu = gcnew Affinity(X->Nu->Item1->Rod, gcnew Dynamic());
 		Rho1->Phi = gcnew Affinity(gcnew Dynamic(), X->Rho->Item1->Cone);
 		Rho1->Rho = gcnew Affinity(N->Rho->Item1->Rod, gcnew Dynamic());
 		Rho1->Sigma = gcnew Affinity(gcnew Dynamic(), N->Sigma->Item1->Cone);
 		Quaternion^ Rho2 = gcnew Quaternion();
-		Rho2->Gamma = gcnew Affinity(R->Gamma->Item2->Rod, R->Gamma->Item2->Cone);
+		Rho2->Gamma = gcnew Affinity(gcnew Dynamic(), R->Gamma->Item2->Cone);
 		Rho2->Nu = gcnew Affinity(gcnew Dynamic(), X->Nu->Item2->Cone);
 		Rho2->Phi = gcnew Affinity(X->Rho->Item2->Rod, gcnew Dynamic());
 		Rho2->Rho = gcnew Affinity(gcnew Dynamic(), N->Rho->Item2->Cone);
