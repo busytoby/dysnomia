b26b512dfad50b6714eefda6a33a93279a903f1f
maria <maria@twee.se>
2022-12-17 10:59:35 -0800

    Prototype Polyrho As A Linked List Of Polygamma Keys And Soliton Values

--- vs2022/Model/Polyrho/Polyrho.h
+++ vs2022/Model/Polyrho/Polyrho.h
@@ -1,9 +1,10 @@
 #pragma once
 
 using namespace System;
+using namespace System::Collections::Generic;
 
 namespace Dysnomia {
-	public ref class Polyrho
+	public ref class Polyrho : LinkedList<KeyValuePair<Polygamma^, Soliton^>>
 	{
 	public:
 		Polyrho(Polysigma^);
