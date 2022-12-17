14cf6494dbf157eafa59a22eeb70fc39f9ff73e8
maria <maria@twee.se>
2022-12-16 20:13:04 -0800

    Define Opal As A Molybdenum

--- vs2022/Prion/Elements/Molybdenum.cs
+++ vs2022/Prion/Elements/Molybdenum.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dysnomia;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,15 @@ using System.Threading.Tasks;
 
 namespace Prion.Elements
 {
-    internal class Molybdenum
+    public class Molybdenum
     {
+        public Chromium Rho;
+        public Sulfur Eta;
+        public Quark Alpha;
+
+        public Molybdenum(Sulfur Mu)
+        {
+            Rho = new Chromium(Mu);
+        }
     }
 }
