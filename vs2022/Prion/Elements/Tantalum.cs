4994474fd9f0d76f4e46bc9322dc9de6970bba70
maria <maria@twee.se>
2022-12-20 22:34:33 -0800

    Proof Of 4 Legged Animals, 19 Chromosomes Complete

--- vs2022/Prion/Elements/Tantalum.cs
+++ vs2022/Prion/Elements/Tantalum.cs
@@ -32,6 +32,28 @@ namespace Prion.Elements
             Pi.Add(Niobium.Ypsilon.Y.Lambda.Xi);
             Pi.Add(Radon.Tau.W.L);
             Pi.Add(Molybdenum.Eta.Xi, Tungsten.Eta.Alpha);
+
+            Tungsten.Rho.Theta.Add(Pi.Mu, Pi.YL.First.Value.Value, Pi.XL.Last.Value.Value, Pi.XL.First.Value.Value);
+            Tungsten.Rho.Theta.Run(Pi, Gamma.Alpha.Phi);
+            Tungsten.Rho.Theta.Run(Pi, Nu.Rho.Gamma);
+            Tungsten.Rho.Theta.Add(Pi.Q);
+            Tungsten.Rho.Theta.Add(Tungsten.Eta.Nu.W.R, Tungsten.Eta.Nu.W.N, Tungsten.Eta.Nu.W.L, Tungsten.Rho.Iota.Upsilon.Lambda.Xi);
+            Tungsten.Rho.Theta.Run(Tungsten.Eta.Nu.Kernel.Eta, Tungsten.Eta.Iota.Upsilon.Phi);
+            Tungsten.Rho.Theta.Run(Tungsten.Eta.Nu.Kernel.Eta, Tungsten.Rho.Iota.Upsilon.Tau);
+            Tungsten.Rho.Theta.Add(Tungsten.Rho.Nu.Kernel.Nu.X);
+            Tungsten.Rho.Theta.Cap(Tungsten.Eta.Nu.Epsilon, Tungsten.Rho.Theta);
+            Tungsten.Rho.Theta.Add(Niobium.Ypsilon.Alpha);
+
+            Tungsten.Eta.Theta.Add(Pi.Mu, Pi.XL.First.Value.Value, Pi.YL.Last.Value.Value, Pi.YL.First.Value.Value);
+            Tungsten.Eta.Theta.Run(Pi, Gamma.Rho.Gamma);
+            Tungsten.Eta.Theta.Run(Pi, Nu.Alpha.Phi);
+            Tungsten.Eta.Theta.Add(Pi.Q);
+            Tungsten.Eta.Theta.Add(Tungsten.Rho.Nu.W.L, Tungsten.Rho.Nu.W.N, Tungsten.Rho.Nu.W.R, Tungsten.Eta.Iota.Upsilon.Lambda.Xi);
+            Tungsten.Eta.Theta.Run(Tungsten.Rho.Nu.Kernel.Eta, Tungsten.Rho.Iota.Upsilon.Phi);
+            Tungsten.Eta.Theta.Run(Tungsten.Rho.Nu.Kernel.Eta, Tungsten.Eta.Iota.Upsilon.Tau);
+            Tungsten.Eta.Theta.Add(Tungsten.Rho.Nu.Kernel.Nu.X);
+            Tungsten.Eta.Theta.Cap(Tungsten.Rho.Nu.Epsilon, Tungsten.Eta.Theta);
+            Tungsten.Eta.Theta.Add(Niobium.Ypsilon.Alpha);
         }
     }
 }
