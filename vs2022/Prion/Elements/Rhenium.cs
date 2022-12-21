8b43aad1600a52eb95b6bd227adecc3ea4e836fc
maria <maria@twee.se>
2022-12-21 09:08:10 -0800

    Proof Of Freeing, First Rhenium At 21 Chromosomes

--- vs2022/Prion/Elements/Rhenium.cs
+++ vs2022/Prion/Elements/Rhenium.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dysnomia;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,41 @@ using System.Threading.Tasks;
 
 namespace Prion.Elements
 {
-    internal class Rhenium
+    public class Rhenium : Tantalum
     {
+        public Rhenium(Octonion Lambda)
+        {
+            if (Vanadium.Beta == null) Vanadium.Beta = Lambda;
+
+            Psi = new Hafnium(Lambda.X.N);
+            Mu = new Hafnium(Lambda.L.Rho);
+
+            Pi = new Soliton(
+                 Vanadium.Beta.L.Rho,
+                 Vanadium.Beta.X,
+                 Vanadium.Beta.L.Nu
+                 );
+
+            Gamma = new Lead(Beta, Mu);
+            Nu = new Lead(Psi, Beta);
+            Phi = new Lead(Psi, Alpha);
+            Rho = new Lead(Mu, Alpha);
+            Sigma = new Lead(Psi, Mu);
+
+            Pi.Add(new Spinor(Gamma.Rho.Gamma, Tungsten.Eta.Nu.Epsilon));
+            Pi.Add(new Spinor(Gamma.Alpha.Nu, Tungsten.Rho.Nu.Epsilon));
+            Pi.Add(Niobium.Ypsilon.Kappa.Tau);
+            Pi.Add(Molybdenum.Eta.Xi, Lambda.X);
+
+            Tungsten.Rho.Theta.Add(Pi.Mu, Pi.XL.First.Value.Value, Pi.YL.Last.Value.Value, Pi.YL.First.Value.Value);
+            Tungsten.Rho.Theta.Run(Pi, Gamma.Alpha.Phi);
+            Tungsten.Rho.Theta.Run(Pi, Sigma.Rho.Sigma);
+            Tungsten.Rho.Theta.Add(Pi.Q);
+
+            Tungsten.Eta.Theta.Add(Pi.Mu, Pi.YL.First.Value.Value, Pi.XL.Last.Value.Value, Pi.YL.First.Value.Value);
+            Tungsten.Eta.Theta.Run(Pi, Nu.Rho.Sigma);
+            Tungsten.Eta.Theta.Run(Pi, Gamma.Alpha.Phi);
+            Tungsten.Eta.Theta.Add(Pi.Q);
+        }
     }
 }
