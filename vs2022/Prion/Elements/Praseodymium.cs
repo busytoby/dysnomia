15dc4ca2fd9a6a09a0939b82c6590cfd2e44f55b
maria <maria@twee.se>
2022-12-21 14:58:51 -0800

    Last 說契 Proof

--- vs2022/Prion/Elements/Praseodymium.cs
+++ vs2022/Prion/Elements/Praseodymium.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dysnomia;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,16 @@ using System.Threading.Tasks;
 
 namespace Prion.Elements
 {
-    internal class Praseodymium
+    public class Praseodymium
     {
+        public Soliton Qi;
+        public Praseodymium(Cerium Delta)
+        {
+            Qi = new Soliton(
+                Delta.Gamma.Alpha.Gamma,
+                new Quark(Cerium.GetQuaternionByName("Pluto"), Vanadium.Rho, Cerium.GetQuaternionByName("Van Allen Belt")),
+                Delta.Sigma.Rho.Sigma
+                );
+        }
     }
 }
