042276b612fad6e304489526a9242fdff68499e8
maria <maria@twee.se>
2022-12-21 16:30:43 -0800

    Proof Of Worms

--- vs2022/Prion/Elements/Praseodymium.cs
+++ vs2022/Prion/Elements/Praseodymium.cs
@@ -11,7 +11,7 @@ namespace Prion.Elements
     {
         static public Soliton Qi;
         static public Spinor Xi;
-        static public Polyrho Chi;
+        static public Polysigma Chi;
 
         public Praseodymium(Cerium Delta)
         {
@@ -35,6 +35,28 @@ namespace Prion.Elements
                 Delta.Sigma.Alpha.Sigma
                 );
 
+            Chi = new Polysigma();
+            Chi.Add(Qi.Mu, Qi.YL.First.Value.Value, Qi.XL.Last.Value.Value, Qi.XL.First.Value.Value);
+            Chi.Run(Qi, Neodymium.Chi.Rho.Alpha.Sigma);
+            Chi.Run(Qi, Neodymium.Chi.Rho.Alpha.Gamma);
+            Chi.Add(Qi.Q);
+            Chi.Add(Tungsten.Eta.Nu.W.R, Tungsten.Eta.Nu.W.N, Tungsten.Eta.Nu.W.L, Tungsten.Rho.Iota.Upsilon.Lambda.Xi);
+            Chi.Run(Tungsten.Eta.Nu.Kernel.Eta, Tungsten.Eta.Iota.Upsilon.Phi);
+            Chi.Run(Tungsten.Eta.Nu.Kernel.Eta, Tungsten.Rho.Iota.Upsilon.Tau);
+            Chi.Add(Tungsten.Rho.Nu.Kernel.Nu.X);
+            Chi.Cap(Tungsten.Eta.Nu.Epsilon, Tungsten.Rho.Theta);
+            Chi.Add(Osmium.Zeta);
+
+            Chi.Add(Qi.Mu, Qi.XL.First.Value.Value, Qi.YL.Last.Value.Value, Qi.YL.First.Value.Value);
+            Chi.Run(Qi, Neodymium.Chi.Rho.Rho.Gamma);
+            Chi.Run(Qi, Neodymium.Chi.Rho.Rho.Sigma);
+            Chi.Add(Qi.Q);
+            Chi.Add(Tungsten.Rho.Nu.W.L, Tungsten.Rho.Nu.W.N, Tungsten.Rho.Nu.W.R, Tungsten.Eta.Iota.Upsilon.Lambda.Xi);
+            Chi.Run(Tungsten.Rho.Nu.Kernel.Eta, Tungsten.Rho.Iota.Upsilon.Phi);
+            Chi.Run(Tungsten.Rho.Nu.Kernel.Eta, Tungsten.Eta.Iota.Upsilon.Tau);
+            Chi.Add(Tungsten.Rho.Nu.Kernel.Nu.X);
+            Chi.Cap(Tungsten.Rho.Nu.Epsilon, Tungsten.Eta.Theta);
+            Chi.Add(Osmium.Zeta);
         }
     }
 }
