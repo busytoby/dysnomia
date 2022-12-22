c918a37618e28efc979bf7e75edaf16ef7895e73
maria <maria@twee.se>
2022-12-21 16:16:01 -0800

    Proof Of Gamma Organism

--- vs2022/Prion/Elements/Praseodymium.cs
+++ vs2022/Prion/Elements/Praseodymium.cs
@@ -9,16 +9,32 @@ namespace Prion.Elements
 {
     public class Praseodymium
     {
-        public Soliton Qi;
+        static public Soliton Qi;
+        static public Spinor Xi;
+        static public Polyrho Chi;
+
         public Praseodymium(Cerium Delta)
         {
             Neodymium Eta = new Neodymium(Delta.Nu);
+
+            if(Qi == null)
+            {
+                if (Xi != null) throw new Exception("Weird");
+                Xi = new Spinor(Delta.Nu.Rho.Phi, Tungsten.Rho.Nu.Epsilon);
+
+                Rhenium.Pi.Add(new Spinor(Neodymium.Chi.Rho.Alpha.Phi, Tungsten.Eta.Nu.Epsilon));
+                Rhenium.Pi.Add(new Spinor(Neodymium.Chi.Nu.Rho.Phi, Tungsten.Rho.Nu.Epsilon));
+                Rhenium.Pi.Add(Xi);
+                Rhenium.Pi.Add(Tungsten.Rho.Nu.W.R, Osmium.Zeta);
+                Rhenium.Pi.Add(Niobium.Ypsilon.Kappa.Tau);
+            }
 
             Qi = new Soliton(
                 Delta.Gamma.Rho.Gamma,
                 new Quark(Vanadium.Rho, Cerium.GetQuaternionByName("Van Allen Belt"), Cerium.GetQuaternionByName("Pluto")),
                 Delta.Sigma.Alpha.Sigma
                 );
+
         }
     }
 }
