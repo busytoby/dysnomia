d803e3df5066e74c7328dd1193e9e134426215f4
maria <maria@twee.se>
2022-12-21 16:47:10 -0800

    Proof Of Diversity

--- vs2022/Prion/Elements/Praseodymium.cs
+++ vs2022/Prion/Elements/Praseodymium.cs
@@ -36,22 +36,22 @@ namespace Prion.Elements
                 );
 
             Chi = new Polysigma();
-            Chi.Add(Qi.Mu, Qi.XL.First.Value.Value, Qi.YL.Last.Value.Value, Qi.YL.First.Value.Value);
+            Chi.Add(Qi.Mu, Qi.YL.First.Value.Value, Qi.XL.Last.Value.Value, Qi.XL.First.Value.Value);
             Chi.Run(Qi, Neodymium.Chi.Rho.Rho.Gamma);
             Chi.Run(Qi, Neodymium.Chi.Rho.Rho.Rho);
             Chi.Add(Qi.Q);
-            Chi.Add(Tungsten.Rho.Nu.W.L, Tungsten.Rho.Nu.W.N, Tungsten.Rho.Nu.W.R, Tungsten.Eta.Iota.Upsilon.Lambda.Xi);
+            Chi.Add(Tungsten.Rho.Nu.W.L, Tungsten.Rho.Nu.W.N, Tungsten.Rho.Nu.W.R, Qi.Mu);
             Chi.Run(Tungsten.Rho.Nu.Kernel.Eta, Tungsten.Eta.Iota.Upsilon.Tau);
             Chi.Run(Tungsten.Rho.Nu.Kernel.Eta, Tungsten.Rho.Iota.Upsilon.Phi);
             Chi.Add(Osmium.Zeta);
             Chi.Cap(Tungsten.Rho.Nu.Epsilon, Tungsten.Eta.Theta);
             Chi.Add(Niobium.Ypsilon.Alpha);
 
-            Chi.Add(Qi.Mu, Qi.YL.First.Value.Value, Qi.XL.Last.Value.Value, Qi.XL.First.Value.Value);
+            Chi.Add(Qi.Mu, Qi.XL.First.Value.Value, Qi.YL.Last.Value.Value, Qi.YL.First.Value.Value);
             Chi.Run(Qi, Neodymium.Chi.Rho.Alpha.Rho);
             Chi.Run(Qi, Neodymium.Chi.Rho.Alpha.Gamma);
             Chi.Add(Qi.Q);
-            Chi.Add(Tungsten.Eta.Nu.W.R, Tungsten.Eta.Nu.W.N, Tungsten.Eta.Nu.W.L, Tungsten.Rho.Iota.Upsilon.Lambda.Xi);
+            Chi.Add(Tungsten.Eta.Nu.W.R, Tungsten.Eta.Nu.W.N, Tungsten.Eta.Nu.W.L, Qi.Mu);
             Chi.Run(Tungsten.Eta.Nu.Kernel.Eta, Tungsten.Rho.Iota.Upsilon.Tau);
             Chi.Run(Tungsten.Eta.Nu.Kernel.Eta, Tungsten.Eta.Iota.Upsilon.Phi);
             Chi.Add(Osmium.Zeta);
