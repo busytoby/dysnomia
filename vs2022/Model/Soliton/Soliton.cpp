1f96ee0b0d625c9323509d85b23c316b7d4bdae7
maria <maria@twee.se>
2022-12-26 15:48:41 -0800

    Shapes

--- vs2022/Model/Soliton/Soliton.cpp
+++ vs2022/Model/Soliton/Soliton.cpp
@@ -32,17 +32,17 @@ namespace Dysnomia {
 		Tensor^ Lambda1 = gcnew Tensor(Delta, Xi);
 
 		Tensor^ Lambda2 = gcnew Tensor(
-			gcnew Polygamma(Epsilon[5]),
-			gcnew Polygamma(Epsilon[9]),
-			gcnew Polygamma(Epsilon[6]),
-			gcnew Polygamma(Epsilon[8]),
-			gcnew Polygamma(Epsilon[7]));
+			gcnew Polygamma(Epsilon[0]),
+			gcnew Polygamma(Epsilon[4]),
+			gcnew Polygamma(Epsilon[1]),
+			gcnew Polygamma(Epsilon[3]),
+			gcnew Polygamma(Epsilon[2]));
 
 		Octonion^ X = gcnew Octonion(Delta, Lambda1);
 		Octonion^ Y = gcnew Octonion(Delta, Lambda2);
 		V->AddLast(KeyValuePair<BigInteger, Octonion^>(X->Epsilon, X));
 		V->AddLast(KeyValuePair<BigInteger, Octonion^>(Y->Epsilon, Y));
-		Q = gcnew Quark(Epsilon[2], Delta->N, Epsilon[7]);
+		Q = Delta;
 	}
 
 	void Soliton::Add(Spinor^ Eta, Polygamma^ Phi) {
@@ -50,16 +50,15 @@ namespace Dysnomia {
 			return;
 		Shift<Quaternion^>^ Epsilon = gcnew Shift<Quaternion^>(Eta->Lambda);
 
-		int i = Epsilon->Count;
 		Spinor^ L = gcnew Spinor(Q->R, Phi);
 		XL->AddFirst(KeyValuePair<BigInteger, Spinor^>(L->Epsilon, L));
 
 		Tensor^ Lambda = gcnew Tensor(
 			Phi,
-			gcnew Polygamma(Epsilon[i-2]),
-			gcnew Polygamma(Epsilon[i-4]),
-			gcnew Polygamma(Epsilon[i-1]),
-			gcnew Polygamma(Epsilon[i-5]));
+			gcnew Polygamma(Epsilon[4]),
+			gcnew Polygamma(Epsilon[1]),
+			gcnew Polygamma(Epsilon[3]),
+			gcnew Polygamma(Epsilon[2]));
 
 		Octonion^ X = gcnew Octonion(Q, Lambda);
 		V->AddLast(KeyValuePair<BigInteger, Octonion^>(X->Epsilon, X));
@@ -71,15 +70,14 @@ namespace Dysnomia {
 			return;
 		Shift<Quaternion^>^ Epsilon = gcnew Shift<Quaternion^>(Eta->Lambda);
 
-		int i = Epsilon->Count;
 		Spinor^ L = gcnew Spinor(Nu->L, Phi);
 		XL->AddFirst(KeyValuePair<BigInteger, Spinor^>(L->Epsilon, L));
 
 		Tensor^ Lambda = gcnew Tensor(
-			gcnew Polygamma(Epsilon[i-5]),
-			gcnew Polygamma(Epsilon[i-1]),
-			gcnew Polygamma(Epsilon[i-4]),
-			gcnew Polygamma(Epsilon[i-2]),
+			gcnew Polygamma(Epsilon[0]),
+			gcnew Polygamma(Epsilon[4]),
+			gcnew Polygamma(Epsilon[1]),
+			gcnew Polygamma(Epsilon[3]),
 			Phi);
 
 		Octonion^ X = gcnew Octonion(Q, Lambda);
