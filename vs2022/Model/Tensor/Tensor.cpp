bad6ed39417b1e7eb2bfd32231919f735f1a4f41
maria <maria@twee.se>
2022-12-26 13:41:33 -0800

    Tensor Changes

--- vs2022/Model/Tensor/Tensor.cpp
+++ vs2022/Model/Tensor/Tensor.cpp
@@ -19,15 +19,15 @@ namespace Dysnomia {
 		Nu->Gamma = gcnew Affinity(Octogamma->Value.Value->Nu->Rod, M->R->Gamma->Cone);
 		Nu->Nu = gcnew Affinity(M->N->Nu->Rod, Octogamma->Value.Value->Nu->Cone);
 		Nu->Phi = gcnew Affinity(Octogamma->Value.Value->Nu->Rod, M->R->Phi->Cone);
-		Nu->Rho = gcnew Affinity(M->N->Rho->Rod, Octogamma->Value.Value->Nu->Cone);
+		Nu->Rho = gcnew Affinity(M->N->Nu->Rod, Octogamma->Value.Value->Rho->Cone);
 		Nu->Sigma = gcnew Affinity(gcnew Dynamic(), M->R->Sigma->Cone);
 		Nu->Epsilon = Math::ModPow(Gamma->Epsilon, Octogamma->Value.Value->Nu->Cone->Manifold, M->L->Nu->Rod->Barn);
 
 		Phi = gcnew Quaternion();
 		Phi->Gamma = gcnew Affinity(Octogamma->Value.Value->Phi->Rod, M->N->Gamma->Cone);
-		Phi->Nu = gcnew Affinity(M->L->Nu->Rod, Octogamma->Value.Value->Phi->Cone);
+		Phi->Nu = gcnew Affinity(M->L->Phi->Rod, Octogamma->Value.Value->Nu->Cone);
 		Phi->Phi = gcnew Affinity(Octogamma->Value.Value->Phi->Rod, M->N->Phi->Cone);
-		Phi->Rho = gcnew Affinity(M->L->Rho->Rod, Octogamma->Value.Value->Phi->Cone);
+		Phi->Rho = gcnew Affinity(M->L->Phi->Rod, Octogamma->Value.Value->Rho->Cone);
 		Phi->Sigma = gcnew Affinity(Octogamma->Value.Value->Phi->Rod, M->N->Sigma->Cone);
 		Phi->Epsilon = Math::ModPow(Nu->Epsilon, Octogamma->Value.Value->Nu->Cone->Manifold, M->L->Nu->Rod->Barn);
 
@@ -50,43 +50,43 @@ namespace Dysnomia {
 
 	Tensor::Tensor(Polygamma^ X, Polygamma^ Y, Polygamma^ Z, Polygamma^ R, Polygamma^ L) {
 		Gamma = gcnew Quaternion();
-		Gamma->Gamma = gcnew Affinity(Y->First->Value.Value->Gamma->Rod, X->First->Value.Value->Gamma->Cone);
-		Gamma->Nu = gcnew Affinity(Z->First->Value.Value->Gamma->Rod, Z->First->Value.Value->Gamma->Cone);
-		Gamma->Phi = gcnew Affinity(L->First->Value.Value->Gamma->Rod, R->First->Value.Value->Gamma->Cone);
-		Gamma->Rho = gcnew Affinity(Z->First->Value.Value->Gamma->Rod, R->First->Value.Value->Gamma->Cone);
-		Gamma->Sigma = gcnew Affinity(L->First->Value.Value->Gamma->Rod, gcnew Dynamic());
-		Gamma->Epsilon = Math::ModPow(R->First->Value.Key, L->First->Value.Value->Nu->Cone->Manifold, Z->First->Value.Value->Nu->Rod->Barn);
+		Gamma->Gamma = gcnew Affinity(Y->Last->Value.Value->Gamma->Rod, X->First->Value.Value->Gamma->Cone);
+		Gamma->Nu = gcnew Affinity(Y->Last->Value.Value->Gamma->Rod, R->Last->Value.Value->Nu->Cone);
+		Gamma->Phi = gcnew Affinity(Y->First->Value.Value->Gamma->Rod, X->Last->Value.Value->Phi->Cone);
+		Gamma->Rho = gcnew Affinity(Y->Last->Value.Value->Gamma->Rod, R->Last->Value.Value->Rho->Cone);
+		Gamma->Sigma = gcnew Affinity(Y->Last->Value.Value->Gamma->Rod, L->First->Value.Value->Sigma->Cone);
+		Gamma->Epsilon = Math::ModPow(Y->First->Value.Key, Y->First->Value.Value->Nu->Cone->Manifold, X->First->Value.Value->Nu->Rod->Barn);
 
 		Nu = gcnew Quaternion();
-		Nu->Gamma = gcnew Affinity(Y->First->Value.Value->Nu->Rod, X->First->Value.Value->Nu->Cone);
-		Nu->Nu = gcnew Affinity(Z->First->Value.Value->Nu->Rod, Z->First->Value.Value->Nu->Cone);
-		Nu->Phi = gcnew Affinity(L->First->Value.Value->Nu->Rod, R->First->Value.Value->Nu->Cone);
-		Nu->Rho = gcnew Affinity(Z->First->Value.Value->Nu->Rod, R->First->Value.Value->Nu->Cone);
-		Nu->Sigma = gcnew Affinity(L->First->Value.Value->Nu->Rod, gcnew Dynamic());
-		Nu->Epsilon = Math::ModPow(Gamma->Epsilon, L->First->Value.Value->Nu->Cone->Manifold, Z->First->Value.Value->Nu->Rod->Barn);
+		Nu->Gamma = gcnew Affinity(Y->Last->Value.Value->Nu->Rod, R->First->Value.Value->Gamma->Cone);
+		Nu->Nu = gcnew Affinity(Z->First->Value.Value->Nu->Rod, Y->Last->Value.Value->Nu->Cone);
+		Nu->Phi = gcnew Affinity(Y->Last->Value.Value->Nu->Rod, R->Last->Value.Value->Phi->Cone);
+		Nu->Rho = gcnew Affinity(Z->First->Value.Value->Nu->Rod, Y->Last->Value.Value->Rho->Cone);
+		Nu->Sigma = gcnew Affinity(L->Last->Value.Value->Nu->Rod, R->First->Value.Value->Sigma->Cone);
+		Nu->Epsilon = Math::ModPow(Gamma->Epsilon, Y->First->Value.Value->Nu->Cone->Manifold, X->First->Value.Value->Nu->Rod->Barn);
 
 		Phi = gcnew Quaternion();
-		Phi->Gamma = gcnew Affinity(Y->First->Value.Value->Phi->Rod, X->First->Value.Value->Phi->Cone);
-		Phi->Nu = gcnew Affinity(Z->First->Value.Value->Phi->Rod, Z->First->Value.Value->Phi->Cone);
-		Phi->Phi = gcnew Affinity(L->First->Value.Value->Phi->Rod, R->First->Value.Value->Phi->Cone);
-		Phi->Rho = gcnew Affinity(Z->First->Value.Value->Phi->Rod, R->First->Value.Value->Phi->Cone);
-		Phi->Sigma = gcnew Affinity(L->First->Value.Value->Phi->Rod, gcnew Dynamic());
-		Phi->Epsilon = Math::ModPow(Nu->Epsilon, L->First->Value.Value->Nu->Cone->Manifold, Z->First->Value.Value->Nu->Rod->Barn);
+		Phi->Gamma = gcnew Affinity(Y->First->Value.Value->Phi->Rod, Z->Last->Value.Value->Gamma->Cone);
+		Phi->Nu = gcnew Affinity(X->Last->Value.Value->Phi->Rod, Y->First->Value.Value->Nu->Cone);
+		Phi->Phi = gcnew Affinity(Y->First->Value.Value->Phi->Rod, Z->Last->Value.Value->Phi->Cone);
+		Phi->Rho = gcnew Affinity(X->Last->Value.Value->Phi->Rod, Y->First->Value.Value->Rho->Cone);
+		Phi->Sigma = gcnew Affinity(Y->First->Value.Value->Phi->Rod, Z->Last->Value.Value->Sigma->Cone);
+		Phi->Epsilon = Math::ModPow(Nu->Epsilon, Y->First->Value.Value->Nu->Cone->Manifold, X->First->Value.Value->Nu->Rod->Barn);
 
 		Rho = gcnew Quaternion();
-		Rho->Gamma = gcnew Affinity(Y->First->Value.Value->Rho->Rod, X->First->Value.Value->Rho->Cone);
-		Rho->Nu = gcnew Affinity(Z->First->Value.Value->Rho->Rod, Z->First->Value.Value->Rho->Cone);
-		Rho->Phi = gcnew Affinity(L->First->Value.Value->Rho->Rod, R->First->Value.Value->Rho->Cone);
-		Rho->Rho = gcnew Affinity(Z->First->Value.Value->Rho->Rod, R->First->Value.Value->Rho->Cone);
-		Rho->Sigma = gcnew Affinity(L->First->Value.Value->Rho->Rod, gcnew Dynamic());
-		Rho->Epsilon = Math::ModPow(Phi->Epsilon, L->First->Value.Value->Nu->Cone->Manifold, Z->First->Value.Value->Nu->Rod->Barn);
+		Rho->Gamma = gcnew Affinity(Y->First->Value.Value->Rho->Rod, Z->Last->Value.Value->Gamma->Cone);
+		Rho->Nu = gcnew Affinity(Y->First->Value.Value->Rho->Rod, X->First->Value.Value->Nu->Cone);
+		Rho->Phi = gcnew Affinity(Z->Last->Value.Value->Rho->Rod, Y->Last->Value.Value->Phi->Cone);
+		Rho->Rho = gcnew Affinity(Y->First->Value.Value->Rho->Rod, R->First->Value.Value->Rho->Cone);
+		Rho->Sigma = gcnew Affinity(Y->Last->Value.Value->Rho->Rod, Z->First->Value.Value->Sigma->Cone);
+		Rho->Epsilon = Math::ModPow(Phi->Epsilon, Y->First->Value.Value->Nu->Cone->Manifold, X->First->Value.Value->Nu->Rod->Barn);
 
 		Sigma = gcnew Quaternion();
-		Sigma->Gamma = gcnew Affinity(Y->First->Value.Value->Sigma->Rod, X->First->Value.Value->Sigma->Cone);
-		Sigma->Nu = gcnew Affinity(Z->First->Value.Value->Sigma->Rod, Z->First->Value.Value->Sigma->Cone);
-		Sigma->Phi = gcnew Affinity(L->First->Value.Value->Sigma->Rod, R->First->Value.Value->Sigma->Cone);
-		Sigma->Rho = gcnew Affinity(Z->First->Value.Value->Sigma->Rod, R->First->Value.Value->Sigma->Cone);
-		Sigma->Sigma = gcnew Affinity(L->First->Value.Value->Sigma->Rod, gcnew Dynamic());
-		Sigma->Epsilon = Math::ModPow(Rho->Epsilon, L->First->Value.Value->Nu->Cone->Manifold, Z->First->Value.Value->Nu->Rod->Barn);
+		Sigma->Gamma = gcnew Affinity(L->First->Value.Value->Sigma->Rod, L->Last->Value.Value->Gamma->Cone);
+		Sigma->Nu = gcnew Affinity(Y->First->Value.Value->Sigma->Rod, L->First->Value.Value->Nu->Cone);
+		Sigma->Phi = gcnew Affinity(L->Last->Value.Value->Sigma->Rod, R->First->Value.Value->Phi->Cone);
+		Sigma->Rho = gcnew Affinity(Y->First->Value.Value->Sigma->Rod, L->First->Value.Value->Rho->Cone);
+		Sigma->Sigma = gcnew Affinity(L->First->Value.Value->Sigma->Rod, L->Last->Value.Value->Sigma->Cone);
+		Sigma->Epsilon = Math::ModPow(Rho->Epsilon, Y->First->Value.Value->Nu->Cone->Manifold, X->First->Value.Value->Nu->Rod->Barn);
 	}
 }
