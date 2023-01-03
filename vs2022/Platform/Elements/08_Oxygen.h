c3886866fb9211b99ddb139ef135459480333b4d
maria <maria@twee.se>
2023-01-02 17:46:50 -0800

    Proof Of Weight

--- vs2022/Platform/Elements/08_Oxygen.h
+++ vs2022/Platform/Elements/08_Oxygen.h
@@ -1,14 +1,17 @@
 #pragma once
 
 using namespace System;
+using namespace System::Numerics;
 
 namespace Dysnomia {
 	namespace Platform {
 		namespace Elements {
 			public ref class Oxygen : public Quark<Epsilon^> {
 			public:
+				property Complex Weight { Complex get() { return (Complex)((Affinity^)N)->Cone->Base / (Complex)((Dynamic^)L)->Element; }};
+
 				Oxygen() : Quark(gcnew Affinity(), gcnew Dynamic(), gcnew Dynamic()) { };
-				Oxygen(Affinity^ N, Dynamic^ R, Dynamic^ L) : Quark(N, R, L) {};
+				Oxygen(Affinity^ N, Dynamic^ R, Dynamic^ L) : Quark(N, R, L) { };
 			};
 		}
 	}
