3f712bfd23eea7222a02c7414791ecf8a9c2314f
maria <maria@twee.se>
2023-01-02 18:57:12 -0800

    Proof Of Bullseye

--- vs2022/Platform/Elements/12_Magnesium.h
+++ vs2022/Platform/Elements/12_Magnesium.h
@@ -14,6 +14,7 @@ namespace Dysnomia {
 				Helium^ Xi;
 				Nitrogen^ Upsilon;
 				Lithium^ Nu;
+				Oxygen^ Rho;
 
 				Magnesium(Neon^ N, Sodium^ S) : Quark(S->Beta->Rod, N->R, N->N) {
 					assert(N->Weight != S->Rho->Weight);
@@ -22,7 +23,10 @@ namespace Dysnomia {
 					Xi = gcnew Helium((Dynamic^)N->L, (Dynamic^)N->R);
 
 					Upsilon = gcnew Nitrogen(gcnew Beryllium(), (Dynamic^)N->L, gcnew Hydrogen());
+
 					Nu = gcnew Lithium(((Affinity^)Upsilon->N)->Rod, ((Affinity^)Upsilon->N)->Cone, (Dynamic^)Upsilon->R);
+					Rho = gcnew Oxygen(gcnew Affinity(gcnew Dynamic(), (Dynamic^)N->R), gcnew Dynamic(), gcnew Dynamic());
+
 				}
 			};
 		}
