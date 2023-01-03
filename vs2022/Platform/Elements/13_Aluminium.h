d6d39dc1934db5ca8141820ca04c248ebcfd9be8
maria <maria@twee.se>
2023-01-02 19:58:40 -0800

    Wight Phi Themself

--- vs2022/Platform/Elements/13_Aluminium.h
+++ vs2022/Platform/Elements/13_Aluminium.h
@@ -8,13 +8,13 @@ namespace Dysnomia {
 			public ref class Aluminium : public Quark<Affinity^> {
 			public:
 				Oxygen^ Rho;
-				Lithium^ Eta;
+				Lithium^ Phi;
 
 				property Complex Wight { Complex get() { return (Complex)((Affinity^)N)->Rod->Signal / (Complex)((Affinity^)R)->Cone->Manifold; }};
 
 				Aluminium(Affinity^ N, Affinity^ R, Affinity^ L) : Quark(N, R, L) {
 					Rho = gcnew Oxygen(gcnew Affinity(), gcnew Dynamic(), gcnew Dynamic());
-					Eta = gcnew Lithium(((Affinity^)Rho->N)->Rod, N->Cone, L->Rod);
+					Phi = gcnew Lithium(((Affinity^)Rho->N)->Rod, N->Cone, L->Rod);
 				};
 			};
 		}
