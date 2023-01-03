da6ec03d8cd2920f0c3b8d0eaea552b1cbb3dd49
maria <maria@twee.se>
2023-01-02 18:17:09 -0800

    Proof Of Sound In A Vacuum

--- vs2022/Platform/Elements/12_Magnesium.h
+++ vs2022/Platform/Elements/12_Magnesium.h
@@ -10,12 +10,19 @@ namespace Dysnomia {
 			public ref class Magnesium : public Quark<Epsilon^>
 			{
 			public:
+				Sodium^ Mu;
 				Helium^ Xi;
+				Nitrogen^ Upsilon;
+				Lithium^ Nu;
 
 				Magnesium(Neon^ N, Sodium^ S) : Quark(S->Beta->Rod, N->R, N->N) {
 					assert(N->Weight != S->Rho->Weight);
 
+					Mu = S;
 					Xi = gcnew Helium((Dynamic^)N->L, (Dynamic^)N->R);
+
+					Upsilon = gcnew Nitrogen(gcnew Beryllium(), (Dynamic^)N->L, gcnew Hydrogen());
+					Nu = gcnew Lithium(((Affinity^)Upsilon->N)->Rod, (Dynamic^)Upsilon->L, ((Affinity^)Upsilon->N)->Cone);
 				}
 			};
 		}
