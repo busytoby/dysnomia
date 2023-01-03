ec4902b736e978dbc0f98a7fb272be877692f7ad
maria <maria@twee.se>
2023-01-02 19:56:06 -0800

    Proof Of Can

--- vs2022/Platform/Elements/13_Aluminium.h
+++ vs2022/Platform/Elements/13_Aluminium.h
@@ -7,9 +7,15 @@ namespace Dysnomia {
 		namespace Elements {
 			public ref class Aluminium : public Quark<Affinity^> {
 			public:
+				Oxygen^ Rho;
+				Lithium^ Eta;
+
 				property Complex Wight { Complex get() { return (Complex)((Affinity^)N)->Rod->Signal / (Complex)((Affinity^)R)->Cone->Manifold; }};
 
-				Aluminium(Affinity^ N, Affinity^ R, Affinity^ L) : Quark(N, R, L) {};
+				Aluminium(Affinity^ N, Affinity^ R, Affinity^ L) : Quark(N, R, L) {
+					Rho = gcnew Oxygen(gcnew Affinity(), gcnew Dynamic(), gcnew Dynamic());
+					Eta = gcnew Lithium(((Affinity^)Rho->N)->Rod, N->Cone, L->Rod);
+				};
 			};
 		}
 	}
