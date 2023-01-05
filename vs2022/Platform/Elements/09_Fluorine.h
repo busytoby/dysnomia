8e57302686163a03aec3b4a4a07efa6d686a5d88
maria <maria@twee.se>
2023-01-04 17:11:49 -0800

    that Xi Zeta Pi this Xi Zeta Pi that Xi Zeta Beta Pi Add that Alpha that Omicron Zeta Alpha Cone Alpha Beta

--- vs2022/Platform/Elements/09_Fluorine.h
+++ vs2022/Platform/Elements/09_Fluorine.h
@@ -20,9 +20,9 @@ namespace Dysnomia {
 					Beryllium^ Alpha = gcnew Beryllium();
 					Affinity^ R = gcnew Affinity(Beta, Alpha->Beta);
 					Boron^ Pi = gcnew Boron(R, Alpha, Alpha->Beta);
-					Xi->Zeta(Beta, Pi);				
-					Xi->Zeta(Pi, that);
 					that->Xi->Zeta(Pi, this);
+					Xi->Zeta(Pi, that);
+					Xi->Zeta(Beta, Pi);				
 					Add(that, Alpha);
 					that->Omicron->Zeta(Alpha->Cone, Alpha->Beta);
 				}
