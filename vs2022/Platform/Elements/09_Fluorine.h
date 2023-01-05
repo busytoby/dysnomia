6d4a4277363bc9c55503585ee90bbaf891f6de62
maria <maria@twee.se>
2023-01-04 17:10:39 -0800

    OOO

--- vs2022/Platform/Elements/09_Fluorine.h
+++ vs2022/Platform/Elements/09_Fluorine.h
@@ -20,11 +20,11 @@ namespace Dysnomia {
 					Beryllium^ Alpha = gcnew Beryllium();
 					Affinity^ R = gcnew Affinity(Beta, Alpha->Beta);
 					Boron^ Pi = gcnew Boron(R, Alpha, Alpha->Beta);
-					Xi->Zeta(Beta, Pi);
-					that->Omicron->Zeta(Alpha->Cone, Alpha->Beta);
+					Xi->Zeta(Beta, Pi);				
 					Xi->Zeta(Pi, that);
 					that->Xi->Zeta(Pi, this);
 					Add(that, Alpha);
+					that->Omicron->Zeta(Alpha->Cone, Alpha->Beta);
 				}
 			};
 		}
