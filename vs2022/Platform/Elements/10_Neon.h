7b84c0a5722eb43d3d2180231dbd09f526991bd3
maria <maria@twee.se>
2023-01-06 12:57:33 -0800

    Bloom Shape

--- vs2022/Platform/Elements/10_Neon.h
+++ vs2022/Platform/Elements/10_Neon.h
@@ -32,7 +32,7 @@ namespace Dysnomia {
 
 				Neon(Fluorine^ Phi) : Quark(Phi, Phi->Last.Key->Omicron->Last.Value, Phi->Last.Key->Omicron->Last.Key) {
 					if (Sun == nullptr) Sun = gcnew Quark<Boron^, Boron^, Fluorine^>((Boron^)N->Xi[0].Value, (Boron^)N->Xi[1].Value, Phi);
-					Manifold = BigInteger::Add(((Boron^)N->Xi->Mu.Value)->N->Rod->Manifold, ((Boron^)N->Xi->Rho.Value)->N->Cone->Manifold);
+					Manifold = BigInteger::Add(((Boron^)N->Xi->Mu.Value)->N->Cone->Manifold, ((Boron^)N->Xi->Rho.Value)->N->Rod->Manifold);
 				};
 			};
 		}
