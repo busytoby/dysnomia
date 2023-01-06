4ba6147cbfd353831586cd3ac1e9fbedc88882ad
maria <maria@twee.se>
2023-01-05 16:30:20 -0800

    Die Ejnessigsnatriumsäure

--- vs2022/Platform/Elements/11_Natrium.h
+++ vs2022/Platform/Elements/11_Natrium.h
@@ -11,21 +11,19 @@ using namespace System::Numerics;
 namespace Dysnomia {
 	namespace Platform {
 		namespace Elements {
-			public ref class Natrium
+			public ref class Natrium : Poly<Oxygen^, Carbon^>
 			{
 			public:
 				Neon^ Pi;
-				Oxygen^ Kappa;
-				Carbon^ Chi;
 
 				Natrium(Fluorine^ F) {
 					Pi = gcnew Neon(F);
-					Chi = gcnew Carbon();
+					Carbon^ Chi = gcnew Carbon();
 					Boron^ Lambda = F->Add(Chi);				
 					Beryllium^ Alpha = gcnew Beryllium();
 					Chi->Omicron->Zeta(Lambda->N->Cone, Alpha->Cone);
 					Chi->Add(Alpha, Chi->Omicron);
-					Kappa = gcnew Oxygen(Pi, Pi->Beta, Alpha->Rod);
+					Oxygen^ Kappa = gcnew Oxygen(Pi, Pi->Beta, Alpha->Rod);
 					F->Xi->Zeta(Kappa, Pi);
 					F->Add(Chi, Alpha);
 					F->Xi->Zeta(Pi, Chi);
@@ -34,6 +32,16 @@ namespace Dysnomia {
 					Boron^ Iota = gcnew Boron(R, Alpha, Alpha->Beta);
 					F->Xi->Zeta(Pi, Iota);
 					F->Add(F[0].Key);
+					Add(Kappa, Chi);
+				}
+
+				BigInteger Grow() {
+					Add(Last.Key, Last.Value);
+					BigInteger T = 0;
+					for (int i = 0; i < Count; i++) {
+						T = BigInteger::Add(T, Last.Value->Beta->Barn);
+					}
+					return T;
 				}
 			};
 		}
