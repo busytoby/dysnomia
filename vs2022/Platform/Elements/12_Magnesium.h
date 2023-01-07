ea960aa15b9e62763980a3eb3782d581b44e7c15
maria <maria@twee.se>
2023-01-06 20:00:02 -0800

    Stub For Magnesium

--- vs2022/Platform/Elements/12_Magnesium.h
+++ vs2022/Platform/Elements/12_Magnesium.h
@@ -5,29 +5,33 @@ using namespace System;
 namespace Dysnomia {
 	namespace Platform {
 		namespace Elements {
-			/*
-			public ref class Magnesium : public Quark<Hydrogen^, Hydrogen^, Beryllium^>
+			public ref class Magnesium : public Poly<Carbon^, Fluorine^>
 			{
 			public:
-				Sodium^ Mu;
-				Helium^ Xi;
-				Nitrogen^ Upsilon;
-				Lithium^ Nu;
-				Oxygen^ Rho;
 
-				Magnesium(Neon^ N, Sodium^ S) : Quark(S->Beta->Rod, N->R, N->N) {
-					assert(N->Weight != S->Rho->Weight);
+				Magnesium(Fluorine^ F, Natrium^ S, Fluorine^ K) {
 
-					Mu = S;
-					Xi = gcnew Helium(N->L, N->R);
-
-					Upsilon = gcnew Nitrogen(gcnew Beryllium(), N->L, gcnew Hydrogen());
-
-					Nu = gcnew Lithium(Upsilon->N->Rod, Upsilon->N->Cone, Upsilon->R);
-					Rho = gcnew Oxygen(gcnew Beryllium(gcnew Hydrogen(), N->R), gcnew Hydrogen(), N->L);
 				}
+				/*
+								Sodium^ Mu;
+								Helium^ Xi;
+								Nitrogen^ Upsilon;
+								Lithium^ Nu;
+								Oxygen^ Rho;
+
+								Magnesium(Neon^ N, Sodium^ S) : Quark(S->Beta->Rod, N->R, N->N) {
+									assert(N->Weight != S->Rho->Weight);
+
+									Mu = S;
+									Xi = gcnew Helium(N->L, N->R);
+
+									Upsilon = gcnew Nitrogen(gcnew Beryllium(), N->L, gcnew Hydrogen());
+
+									Nu = gcnew Lithium(Upsilon->N->Rod, Upsilon->N->Cone, Upsilon->R);
+									Rho = gcnew Oxygen(gcnew Beryllium(gcnew Hydrogen(), N->R), gcnew Hydrogen(), N->L);
+								}
+											*/
 			};
-			*/
 		}
 	}
 }
