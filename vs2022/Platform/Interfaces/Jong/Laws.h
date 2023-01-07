38b34a2ffab6e79fa50b9aecd6fd5a1577c1b328
maria <maria@twee.se>
2023-01-07 15:13:46 -0800

    Legal Proof Of Capital

--- vs2022/Platform/Interfaces/Jong/Laws.h
+++ vs2022/Platform/Interfaces/Jong/Laws.h
@@ -10,6 +10,7 @@ namespace Dysnomia {
 			public:
 				static property BigInteger First { BigInteger get() { return BigInteger::Add(Constants::Mu, Constants::Evidences[0]); }};
 				static property BigInteger Second { BigInteger get() { return BigInteger::ModPow(Dysnomia::Platform::Wou::Laws::Fourth, Constants::Mu, Constants::Rho); }};
+				static property BigInteger Third { BigInteger get() { return BigInteger::ModPow(Second, Constants::Rho, Constants::Mu); }};
 			};
 		}
 	}
