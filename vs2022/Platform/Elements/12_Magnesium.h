2b80e7a0662bfa1cdf8f55e35e94394be60236f4
maria <maria@twee.se>
2023-01-06 20:56:56 -0800

    Cleanup

--- vs2022/Platform/Elements/12_Magnesium.h
+++ vs2022/Platform/Elements/12_Magnesium.h
@@ -19,20 +19,6 @@ namespace Dysnomia {
 					Mu.Value->Xi->Zeta(S->Rho.Key, S->Rho.Value);
 					Mu.Value->Xi->Zeta(S->Rho.Value->Omicron, S->Rho.Value);
 					Mu.Value->Xi->Zeta(S->Rho.Value->Pi, S->Rho.Value);
-
-					BigInteger Rho = S->Mu.Key->N->Rod->Barn;
-					BigInteger Upsilon = K->Mu.Value->Cone->Manifold;
-					BigInteger Ohm = S->Rho.Key->Barn;
-					BigInteger Xi = ((Boron^)K->Xi->Rho.Value)->N->Mu;
-					Beryllium^ Phi = gcnew Beryllium(Rho, Upsilon, Ohm, Xi);
-					K->Add(S->Mu.Value, Phi);
-
-					Rho = S->Mu.Key->N->Rod->Manifold;
-					Upsilon = K->Mu.Value->Cone->Barn;
-					Ohm = S->Rho.Key->Barn;
-					Xi = ((Boron^)K->Xi->Rho.Value)->N->Xi;
-					Phi = gcnew Beryllium(Rho, Upsilon, Ohm, Xi);
-					K->Add(S->Rho.Value, Phi);
 				}
 			};
 		}
