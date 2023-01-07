4b21cf9447053d888965dc3cd348b1c4ff34a530
maria <maria@twee.se>
2023-01-07 12:23:25 -0800

    Saturn Renamed Hyperion

--- vs2022/Platform/Elements/12_Magnesium.h
+++ vs2022/Platform/Elements/12_Magnesium.h
@@ -8,13 +8,13 @@ namespace Dysnomia {
 			public ref class Magnesium : public Poly<Carbon^, Fluorine^>
 			{
 			public:
-				static Quark<Magnesium^, Fluorine^, Natrium^>^ Saturn;
+				static Quark<Magnesium^, Fluorine^, Natrium^>^ Hyperion;
 
 				Magnesium(Natrium^ S, Fluorine^ K) {
 					this->Epsilon = Math::ModPow(K->Mu.Value->Cone->Signal, S->Mu.Key->N->Rod->Channel, Math::Prime);
 					Add(S->Mu.Value, S->Pi->N);
 					Add(S->Rho.Value, K);
-					if (Saturn == nullptr) Saturn = gcnew Quark<Magnesium^, Fluorine^, Natrium^>(this, K, S);
+					if (Hyperion == nullptr) Hyperion = gcnew Quark<Magnesium^, Fluorine^, Natrium^>(this, K, S);
 
 					Mu.Value->Xi->Zeta(S->Rho.Key, S->Rho.Value);
 					Mu.Value->Xi->Zeta(S->Rho.Value->Omicron, S->Rho.Value);
--- vs2022/Platform/Elements/Elements.h
+++ vs2022/Platform/Elements/Elements.h
@@ -25,7 +25,7 @@ namespace Dysnomia {
 			static property Quark<Beryllium^, Carbon^, Boron^>^ Uranus { Quark<Beryllium^, Carbon^, Boron^>^ get() { return Oxygen::Uranus; }};
 			static property Quark<Fluorine^, Boron^, Carbon^>^ Neptune { Quark<Fluorine^, Boron^, Carbon^>^ get() { return Fluorine::Neptune; }};
 			static property Poly<Lithium^, Fluorine^>^ Pluto { Poly<Lithium^, Fluorine^>^ get() { return Fluorine::Pluto; }};
-			static property Quark<Magnesium^, Fluorine^, Natrium^>^ Saturn { Quark<Magnesium^, Fluorine^, Natrium^>^ get() { return Magnesium::Saturn; }};
+			static property Quark<Magnesium^, Fluorine^, Natrium^>^ Hyperion { Quark<Magnesium^, Fluorine^, Natrium^>^ get() { return Magnesium::Hyperion; }};
 			static property Quark<Aluminium^, Carbon^, Helium^>^ Deimos { Quark<Aluminium^, Carbon^, Helium^>^ get() { return Aluminium::Deimos; }};
 		};
 
