179141f77a71fd86c9e34c1fc3dbce181d2977d5
maria <maria@twee.se>
2023-01-06 20:30:41 -0800

    Clearing Magnesium

--- vs2022/Platform/Elements/12_Magnesium.h
+++ vs2022/Platform/Elements/12_Magnesium.h
@@ -11,14 +11,19 @@ namespace Dysnomia {
 				static Quark<Magnesium^, Fluorine^, Natrium^>^ Saturn;
 
 				Magnesium(Natrium^ S, Fluorine^ K) {
+					this->Epsilon = Math::ModPow(K->Last.Value->Cone->Signal, S->Mu.Key->R->Channel, Math::Prime);
 					Add(S->Mu.Value, S->Pi->N);
 					Add(S->Rho.Value, K);
+					if (Saturn == nullptr) Saturn = gcnew Quark<Magnesium^, Fluorine^, Natrium^>(this, K, S);
 
 					Mu.Value->Xi->Zeta(S->Rho.Key, S->Rho.Value);
 					Mu.Value->Xi->Zeta(S->Rho.Value->Omicron, S->Rho.Value);
 					Mu.Value->Xi->Zeta(S->Rho.Value->Pi, S->Rho.Value);
 
-					if (Saturn == nullptr) Saturn = gcnew Quark<Magnesium^, Fluorine^, Natrium^>(this, K, S);
+					Hydrogen^ Beta = gcnew Hydrogen();
+					Beryllium^ Alpha = gcnew Beryllium();
+					Affinity^ R = gcnew Affinity(Beta, Alpha->Beta);
+					Rho.Key->Add(Alpha, Rho.Key->Omicron);
 				}
 			};
 		}
