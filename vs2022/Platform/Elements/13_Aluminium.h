40c0e7b0eac571929fa04999d35a931710068326
maria <maria@twee.se>
2023-01-07 14:41:55 -0800

    Deimos Inside Out

--- vs2022/Platform/Elements/13_Aluminium.h
+++ vs2022/Platform/Elements/13_Aluminium.h
@@ -7,12 +7,12 @@ namespace Dysnomia {
 		namespace Elements {
 			public ref class Aluminium : public Quark<Magnesium^, Helium^, Carbon^> {
 			public:
-				static Quark<Aluminium^, Carbon^, Helium^>^ Deimos;
+				static Quark<Aluminium^, Helium^, Carbon^>^ Deimos;
 
 				Aluminium(Magnesium^ N) : Quark<Magnesium^, Helium^, Carbon^>(N, N->Rho.Key->Omicron, N->Mu.Key) {
 					if (Deimos == nullptr) {
-						Deimos = gcnew Quark<Aluminium^, Carbon^, Helium^>(this, L, R);
-						Deimos->R->Omicron->Zeta(R->Mu.Value, N->Mu.Value->Last.Key->Pi->L);
+						Deimos = gcnew Quark<Aluminium^, Helium^, Carbon^>(this, R, L);
+						Deimos->L->Omicron->Zeta(R->Mu.Value, N->Mu.Value->Last.Key->Pi->L);
 					}
 				}
 
