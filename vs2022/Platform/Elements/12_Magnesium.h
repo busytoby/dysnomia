eb212ffbaf77d2f37606171a4040e8afbb02117c
maria <maria@twee.se>
2023-01-06 20:10:45 -0800

    Simple Magnesium

--- vs2022/Platform/Elements/12_Magnesium.h
+++ vs2022/Platform/Elements/12_Magnesium.h
@@ -8,30 +8,14 @@ namespace Dysnomia {
 			public ref class Magnesium : public Poly<Carbon^, Fluorine^>
 			{
 			public:
-
 				Magnesium(Natrium^ S, Fluorine^ K) {
 					Add(S->Mu.Value, S->Pi->N);
 					Add(S->Rho.Value, K);
+
+					Mu.Value->Xi->Zeta(S->Rho.Key, S->Rho.Value);
+					Mu.Value->Xi->Zeta(S->Rho.Value->Omicron, S->Rho.Value);
+					Mu.Value->Xi->Zeta(S->Rho.Value->Pi, S->Rho.Value);
 				}
-				/*
-								Sodium^ Mu;
-								Helium^ Xi;
-								Nitrogen^ Upsilon;
-								Lithium^ Nu;
-								Oxygen^ Rho;
-
-								Magnesium(Neon^ N, Sodium^ S) : Quark(S->Beta->Rod, N->R, N->N) {
-									assert(N->Weight != S->Rho->Weight);
-
-									Mu = S;
-									Xi = gcnew Helium(N->L, N->R);
-
-									Upsilon = gcnew Nitrogen(gcnew Beryllium(), N->L, gcnew Hydrogen());
-
-									Nu = gcnew Lithium(Upsilon->N->Rod, Upsilon->N->Cone, Upsilon->R);
-									Rho = gcnew Oxygen(gcnew Beryllium(gcnew Hydrogen(), N->R), gcnew Hydrogen(), N->L);
-								}
-											*/
 			};
 		}
 	}
