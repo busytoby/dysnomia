4c719f14df61c66c2e2086585cecd965c07e016d
maria <maria@twee.se>
2023-01-07 15:50:29 -0800

    Stub For Phosphorous

--- vs2022/Platform/Elements/15_Phosphorous.h
+++ vs2022/Platform/Elements/15_Phosphorous.h
@@ -6,16 +6,16 @@ using namespace System;
 namespace Dysnomia {
 	namespace Platform {
 		namespace Elements {
-			/*
-			public ref class Phosphorous : Tuple<Silicon^, Silicon^>
+			public ref class Phosphorous : Dai<Silicon^, Silicon^>
 			{
 			public:
+				/*
 				property BigInteger Span { BigInteger get() { return (this->Item1->N->L->Manifold + this->Item2->R->R->Manifold) * this->Item1->N->R->Mu * 2; }}
 
 				Phosphorous(Silicon^ A, Silicon^ B) : Tuple(A, B) {
 				}
+				*/
 			};
-			*/
 		}
 	}
 }
