6b7d0a5fe2ef631edc1cd2f5ea6bafcf57265d3e
maria <maria@twee.se>
2023-01-08 13:27:20 -0800

    The Tenth Law Of Jong Is Leniency

--- vs2022/Platform/Interfaces/Jong/Laws.h
+++ vs2022/Platform/Interfaces/Jong/Laws.h
@@ -12,11 +12,12 @@ namespace Dysnomia {
 				static property BigInteger Second { BigInteger get() { return BigInteger::ModPow(Dysnomia::Platform::Wou::Laws::Fourth, Constants::Mu, Constants::Rho); }};
 				static property BigInteger Third { BigInteger get() { return BigInteger::ModPow(Second, Constants::Rho, Constants::Mu); }};
 				static property BigInteger Fourth { BigInteger get() { return BigInteger::ModPow(Dysnomia::Platform::Wou::Constants::Upsilon, Constants::Psi, Third); }};
-				static property BigInteger Fifth { BigInteger get() { return BigInteger::Add(Constants::Psi, Constants::Last); }};
-				static property BigInteger Sixth { BigInteger get() { return BigInteger::ModPow(Constants::Psi, Constants::Last, Dysnomia::Platform::Wou::Laws::Sixth); }};
+				static property BigInteger Fifth { BigInteger get() { return BigInteger::Add(Constants::Psi, Constants::Rho); }};
+				static property BigInteger Sixth { BigInteger get() { return BigInteger::ModPow(Constants::Psi, Constants::Rho, Dysnomia::Platform::Wou::Laws::Sixth); }};
 				static property BigInteger Seventh { BigInteger get() { return BigInteger::ModPow(Constants::Psi, Constants::Rho, Constants::Nu); }};
 				static property BigInteger Eighth { BigInteger get() { return BigInteger::ModPow(Constants::Psi, Constants::Eta, Constants::Nu); }};
-				static property BigInteger Ninth { BigInteger get() { return BigInteger::ModPow(Constants::Sigma, Eighth, Constants::Last); }};
+				static property BigInteger Ninth { BigInteger get() { return BigInteger::ModPow(Constants::Sigma, Eighth, Constants::Rho); }};
+				static property BigInteger Tenth { BigInteger get() { return BigInteger::ModPow(Constants::Last, Ninth, Constants::Rho); }};
 			};
 		}
 	}
