46ba94a67f1b5627160232a523475a3d05e0c467
maria <maria@twee.se>
2023-01-08 17:49:35 -0800

    Vision Of Tooth

--- vs2022/Platform/Elements/20_Calcium.h
+++ vs2022/Platform/Elements/20_Calcium.h
@@ -1,1 +1,14 @@
 #pragma once
+
+using namespace System;
+
+namespace Dysnomia {
+	namespace Platform {
+		namespace Elements {
+			public ref class Calcium : Dai<Potassium^, Argon^>
+			{
+			public:
+			};
+		}
+	}
+}
