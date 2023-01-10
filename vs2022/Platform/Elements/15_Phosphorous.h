a7a9839ab8d05adc2f78a5a53d84f1f260d3ec8b
maria <maria@twee.se>
2023-01-10 09:38:33 -0800

    Phosphorous Function Renamed Retain

--- vs2022/Platform/Elements/15_Phosphorous.h
+++ vs2022/Platform/Elements/15_Phosphorous.h
@@ -15,14 +15,14 @@ namespace Dysnomia {
 				Phosphorous(Aluminium^ Mu, BigInteger Rho, BigInteger Upsilon, BigInteger Ohm, BigInteger Qi) {
 					Silicon^ Alpha = gcnew Silicon(Mu, Rho, Upsilon, Ohm, Qi);
 					Add(Alpha->Mu.Value, Alpha);
-					Remember(Qi, Alpha);
-					Remember(Rho, Alpha);
-					Remember(Ohm, Alpha);
+					Retain(Qi, Alpha);
+					Retain(Rho, Alpha);
+					Retain(Ohm, Alpha);
 					if (Jupiter19 == nullptr) Jupiter19 = this;
 					if (Thalassa == nullptr) Thalassa = gcnew Quark<Silicon^, Coronium^, Aluminium^>(Alpha, Alpha->Mu.Value, Mu);
 				}
 
-				Coronium^ Remember(BigInteger Qi, Silicon^ Alpha) {
+				Coronium^ Retain(BigInteger Qi, Silicon^ Alpha) {
 					Coronium^ Iota = Alpha->Consider(Qi);
 					Add(Iota, Alpha);
 					return Iota;
