1135e1599127c001bcffef58750d1dba2dd29eb3
maria <maria@twee.se>
2023-01-09 18:49:58 -0800

    Cleanup

--- vs2022/Platform/Elements/Elements.h
+++ vs2022/Platform/Elements/Elements.h
@@ -37,6 +37,7 @@ namespace Dysnomia {
 			static property Quark<Fluorine^, Boron^, Carbon^>^ Neptune { Quark<Fluorine^, Boron^, Carbon^>^ get() { return Fluorine::Neptune; }};
 			static property Dai<Lithium^, Fluorine^>^ Tethys { Dai<Lithium^, Fluorine^>^ get() { return Fluorine::Tethys; }};
 			static property Quark<Magnesium^, Fluorine^, Natrium^>^ VanAllenBelt { Quark<Magnesium^, Fluorine^, Natrium^>^ get() { return Magnesium::VanAllenBelt; }};
+			static property Quark<Boron^, Fluorine^, Magnesium^>^ Dione { Quark<Boron^, Fluorine^, Magnesium^>^ get() { return Magnesium::Dione; }};
 			static property Quark<Aluminium^, Helium^, Carbon^>^ Phobos { Quark<Aluminium^, Helium^, Carbon^>^ get() { return Aluminium::Phobos; }};
 			static property Dai<Carbon^, Coronium^>^ Enceladus { Dai<Carbon^, Coronium^>^ get() { return Silicon::Enceladus; }};
 			static property Dai<Coronium^, Silicon^>^ Jupiter19 { Dai<Coronium^, Silicon^>^ get() { return Phosphorous::Jupiter19; }};
