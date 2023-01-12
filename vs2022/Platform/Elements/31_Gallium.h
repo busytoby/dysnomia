8ff053bcc43aa0dedf374d8c72b40975ae64fa0e
maria <maria@twee.se>
2023-01-11 21:34:14 -0800

    cleanup

--- vs2022/Platform/Elements/31_Gallium.h
+++ vs2022/Platform/Elements/31_Gallium.h
@@ -13,7 +13,7 @@ namespace Dysnomia {
 					Add(Alpha->N->Mu.Key->N->Mu.Value->Mu.Key, Alpha);
 					for (int i = 0; i < Alpha->N->Mu.Value->Mu.Key->Count; i++) {
 						Add(Alpha->N->Mu.Value->Mu.Key[i].Value->Mu.Key->N->N->N->Mu.Key, Alpha);
-						Add(Alpha->N->Mu.Value->Mu.Key[0].Value->Mu.Key->N->N->L, Alpha);
+						Add(Alpha->N->Mu.Value->Mu.Key[i].Value->Mu.Key->N->N->L, Alpha);
 						for (int j = 0; j < Alpha->N->Mu.Value->Mu.Key[i].Value->Mu.Key->N->L->Count; j++)
 							Add(Alpha->N->Mu.Value->Mu.Key[i].Value->Mu.Key->N->L[j].Key, Alpha);
 					}				
