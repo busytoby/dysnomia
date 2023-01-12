ed7f647d327b8dfda993299311f26e7cc43bbe71
maria <maria@twee.se>
2023-01-12 14:12:58 -0800

    Stub The Xun Xibe

---
+++ vs2022/Platform/Interfaces/Xun/Constants.h
@@ -0,0 +1,13 @@
+#pragma once
+
+using namespace Dysnomia::Platform::Elements;
+using namespace System;
+using namespace System::Collections::Generic;
+using namespace System::Numerics;
+
+namespace Dysnomia {
+	namespace Platform {
+		namespace Xun {
+		}
+	}
+}
---
+++ vs2022/Platform/Interfaces/Xun/Xun.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+
+#include "Xun.h"
+
+using namespace Dysnomia::Platform::Elements;
+using namespace Dysnomia::Platform::Xun;
+
+namespace Dysnomia {
+	namespace Platform {
+		namespace Interfaces {
+			Xun::Xun() {
+				Mu = gcnew Tian();
+			}
+		}
+	}
+}
