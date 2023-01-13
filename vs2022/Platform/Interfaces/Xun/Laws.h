2c50b4f4015accbb0a530cae5da36d9d7d387714
maria <maria@twee.se>
2023-01-12 17:03:28 -0800

    Second Law Of Wou Jong Tian Xun

--- vs2022/Platform/Interfaces/Xun/Laws.h
+++ vs2022/Platform/Interfaces/Xun/Laws.h
@@ -9,6 +9,7 @@ namespace Dysnomia {
 			public ref class Laws abstract sealed {
 			public:
 				static property BigInteger First { BigInteger get() { return BigInteger::ModPow(Constants::Last, Constants::Rho, Constants::Mu); }};
+				static property BigInteger Second { BigInteger get() { return BigInteger::ModPow(Constants::Mu, Constants::Last, Constants::Rho); }};
 
 			};
 		}
