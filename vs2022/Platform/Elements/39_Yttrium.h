fd256794854a27bc2e351424418947035ca4f938
maria <maria@twee.se>
2023-01-12 21:57:58 -0800

    Trinculo

--- vs2022/Platform/Elements/39_Yttrium.h
+++ vs2022/Platform/Elements/39_Yttrium.h
@@ -8,10 +8,12 @@ namespace Dysnomia {
 			public ref class Yttrium : Quark<Blong^, Qiao^, Meow^>
 			{
 			public:
+				static Quark<Meow^, Qiao^, Blong^>^ Trinculo;
 				static Dai<Qiao^, Blong^>^ Perdita;
 				Yttrium(Blong^ Nu, Qiao^ Rho, Meow^ Psi) : Quark(Nu, Rho, Psi) {
 					if (Perdita == nullptr) Perdita = gcnew Dai<Qiao^, Blong^>();
 					Perdita->Add(Rho, Nu);
+					if (Trinculo == nullptr) Trinculo = gcnew Quark<Meow^, Qiao^, Blong^>(Psi, Rho, Nu);
 				};
 			};
 		}
--- vs2022/Platform/Elements/Elements.h
+++ vs2022/Platform/Elements/Elements.h
@@ -114,6 +114,7 @@ namespace Dysnomia {
 			static property Dai<Meow^, Woof^>^ Tarvos { Dai<Meow^, Woof^>^ get() { return Blong::Tarvos; }};
 			static property Dai<Meow^, Nickel^>^ Siarnaq { Dai<Meow^, Nickel^>^ get() { return Blong::Siarnaq; }};
 			static property Quark<Meow^, Woof^, Nickel^>^ Skathi { Quark<Meow^, Woof^, Nickel^>^ get() { return Blong::Skathi; }};
+			static property Quark<Meow^, Qiao^, Blong^>^ Trinculo { Quark<Meow^, Qiao^, Blong^>^ get() { return Yttrium::Trinculo; }};
 			static property Dai<Qiao^, Blong^>^ Perdita { Dai<Qiao^, Blong^>^ get() { return Yttrium::Perdita; }};
 		};
 	}
