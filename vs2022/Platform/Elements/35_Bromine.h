25fe22f6244fb341a6d7de94ca8a4fb38c486b85
maria <maria@twee.se>
2023-01-12 16:30:50 -0800

    Nomenclature

--- vs2022/Platform/Elements/35_Bromine.h
+++ vs2022/Platform/Elements/35_Bromine.h
@@ -10,12 +10,20 @@ namespace Dysnomia {
 			public ref class Bromine : Dai<Selenium^, Boron^>
 			{
 			public:
-				Bromine(Arsenic^ Eta, Copper^ Nu) {
-					Nickel^ Rho = Nu->Delta();
-					Sulfur^ Theta = Nu->Mu.Value->Mu.Value->Mu.Key;
-					Chlorine^ Tau = gcnew Chlorine(Theta, Rho->Mu.Value);
-					Argon^ Sigma = gcnew Argon(Theta, Tau, Rho->Mu.Value);
-					Selenium^ Psi = gcnew Selenium(Eta, Rho, Sigma);
+				Sulfur^ Theta;
+				Chlorine^ Tau;
+
+				Copper^ Beta;
+				Nickel^ Pi;
+				Argon^ Zeta;
+
+				Bromine(Arsenic^ Omicron, Copper^ Lambda) {
+					Beta = Lambda;
+					Pi = Beta->Delta();
+					Theta = Beta->Mu.Value->Mu.Value->Mu.Key;
+					Tau = gcnew Chlorine(Theta, Pi->Mu.Value);
+					Zeta = gcnew Argon(Theta, Tau, Pi->Mu.Value);
+					Selenium^ Psi = gcnew Selenium(Omicron, Pi, Zeta);
 					Add(Psi, Theta->N->L->Pi);
 				}
 			};
