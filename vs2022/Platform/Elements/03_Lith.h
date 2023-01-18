54178ea91bebd7e19a66dcd13ded1710311e9feb
maria <maria@twee.se>
2023-01-18 14:59:59 -0800

    Schüler and Schmidt's Dipole & Quadrupole.. Trying To Move On ..

--- vs2022/Platform/Elements/03_Lith.h
+++ vs2022/Platform/Elements/03_Lith.h
@@ -9,9 +9,18 @@ namespace Dysnomia {
 		namespace Elements {
 			public ref class Lith : public Dai<Hadron^, Hel^> {
 			public:
+				static Chi<Hadron^, Hel^>^ Dipole;
+				static Tuple<Chi<Hadron^, Hel^>^, Chi<Hadron^, Hel^>^>^ Quadrupole;
+
 				Lith(Hadron^ Beta) {
 					Add(Beta, gcnew Hel());
+					if (Dipole == nullptr) Dipole = Mu;					
 					Double M = Mass;
+				}
+
+				void Add(Hadron^ Beta, Hel^ Pi) {
+					this->Dai<Hadron^, Hel^>::Add(Beta, Pi);
+					if (Count == 2) Quadrupole = gcnew Tuple<Chi<Hadron^, Hel^>^, Chi<Hadron^, Hel^>^>(Mu, Rho);
 				}
 			};
 		}
