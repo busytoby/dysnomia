2bccbf34ac22c0bc677344837a2f4d1d6c846eeb
maria <maria@twee.se>
2023-01-20 11:20:23 -0800

    Presuming 4 Hadron Cones Will Shake With 1 Hadron Rod To Form The Baryonic Barn

--- vs2022/Platform/Elements/08_Baryon.h
+++ vs2022/Platform/Elements/08_Baryon.h
@@ -11,12 +11,19 @@ namespace Dysnomia {
 
 				Baryon(Down^ Beta) {
 					Hel^ Delta = gcnew Hel(
-						Beta[0]->I[1]->L[0]->I->Base,
-						Beta[0]->I[1]->I->Secret,
-						Beta[0]->L[0]->L[0]->I->Signal,
+						Beta[0]->L[0]->L[6]->L[0]->L[0]->L->Base,
+						Beta[0]->L[0]->L[5]->L[0]->L[0]->L->Secret,
+						Beta[0]->L[0]->L[2]->L[0]->L[0]->L->Signal,
 						Beta[0]->L[0]->L[1]->L[0]->I->Channel);
 
 					Add(Delta, Beta);
+
+					Dynamic^ A = Beta[0]->L[0]->L[1]->L[0]->I;
+					Dynamic^ B = Beta[0]->L[0]->L[6]->L[0]->L[0]->L;
+					Dynamic^ C = Beta[0]->L[0]->L[5]->L[0]->L[0]->L;
+					Dynamic^ D = Beta[0]->L[0]->L[2]->L[0]->L[0]->L;
+					Dynamic^ E = gcnew Dynamic();
+
 
 					if (Hippocamp == nullptr) Hippocamp = gcnew Quark<Nucleon^, Fermion^, Hadron^>(Beta[0]->L, Beta[0]->L[0]->L, Delta[0]->L);
 				}
