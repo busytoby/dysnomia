904a326d1b5f8f7455b9baf7502edbd0966e8b8e
maria <maria@twee.se>
2023-01-21 09:48:40 -0800

    Jupiter19 & Thalassa On Parity

--- vs2022/Platform/Elements/15_Parity.h
+++ vs2022/Platform/Elements/15_Parity.h
@@ -8,32 +8,17 @@ namespace Dysnomia {
 			public ref class Parity : public Dai<Tauon^, Compensation^>
 			{
 			public:
-//				static Dai<Coronium^, Silicon^>^ Jupiter19;
-//				static Edge<Silicon^, Coronium^, Aluminium^>^ Thalassa;
+				static Parity^ Jupiter19;
+				static Edge<Compensation^, Tauon^, Aluminium^>^ Thalassa;
 
 				Parity(Aluminium^ Beta) {			
 					Compensation^ Rho = gcnew Compensation(Beta);
 					for (int i = 0; i < Rho->Count; i++)
 						Add(Rho[i]->L, Rho);
-				}
-/*
-				Phosphorous(Aluminium^ Mu, Int64 Rho, Int64 Upsilon, Int64 Ohm, Int64 Qi) {
-					Silicon^ Alpha = gcnew Silicon(Mu, Rho, Upsilon, Ohm, Qi);
-					for (int i = 0; i < Alpha->Count; i++)
-						Add(Alpha[i].Value, Alpha);
-					Retain(Qi, Alpha);
-					Retain(Rho, Alpha);
-					Retain(Ohm, Alpha);
-					if (Jupiter19 == nullptr) Jupiter19 = this;
-					if (Thalassa == nullptr) Thalassa = gcnew Edge<Silicon^, Coronium^, Aluminium^>(Alpha, Alpha->Mu.Value, Mu);
-				}
 
-				Coronium^ Retain(Int64 Qi, Silicon^ Alpha) {
-					Coronium^ Iota = Alpha->Contain(Qi);
-					Add(Iota, Alpha);
-					return Iota;
+					if (Jupiter19 == nullptr) Jupiter19 = this;
+					if (Thalassa == nullptr) Thalassa = gcnew Edge<Compensation^, Tauon^, Aluminium^>(Rho, Rho[0]->L, Beta);
 				}
-*/
 			};
 		}
 	}
