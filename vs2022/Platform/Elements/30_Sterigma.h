e8b02da0d7555b147936397ee6cad40c5f56d0c3
maria <maria@twee.se>
2023-01-23 14:54:03 -0800

    Stubbing Sterigma

--- vs2022/Platform/Elements/30_Sterigma.h
+++ vs2022/Platform/Elements/30_Sterigma.h
@@ -1,30 +1,26 @@
 #pragma once
-#include "13_Aluminium.h"
-#include "18_Argon.h"
-#include "29_Copper.h"
-#include "28_Nickel.h"
 
 using namespace System;
 
 namespace Dysnomia {
 	namespace Platform {
 		namespace Elements {
-			public ref class Zinc : public Edge<Copper^, Nickel^, Nickel^> {
+			public ref class Sterigma : public Edge<Peridium^, Conidium^, Peridium^> {
 			public:
-				static Dai<Zinc^, Argon^>^ Venus;
+				static Dai<Sterigma^, Argon^>^ Venus;
 				static Edge<Nickel^, Argon^, Natrium^>^ Neith;
-				static Edge<Nickel^, Argon^, Zinc^>^ Callisto;
-				static Edge<Argon^, Zinc^, Aluminium^>^ Neso;
+				static Edge<Nickel^, Argon^, Sterigma^>^ Callisto;
+				static Edge<Argon^, Sterigma^, Aluminium^>^ Neso;
 
-				Zinc(Copper^ Nu, Nickel^ Eta) : Edge(Nu, Eta, Nu->Mu.Value) {
-					if (Venus == nullptr) Venus = gcnew Dai<Zinc^, Argon^>();
+				Sterigma(Copper^ Nu, Nickel^ Eta) : Edge(Nu, Eta, Nu->Mu.Value) {
+					if (Venus == nullptr) Venus = gcnew Dai<Sterigma^, Argon^>();
 					Aluminium^ Theta = gcnew Aluminium(Eta->Mu.Value->Mu.Key->N->N);
 					Nu->Add(Theta, Eta);
 					Argon^ Rho = gcnew Argon(Nu->Mu.Value->Mu.Value->Mu.Key, gcnew Chlorine(Nu->Mu.Value->Mu.Value->Mu.Key), Nu->Mu.Value->Mu.Value);
 					if (Neith == nullptr) Neith = gcnew Edge<Nickel^, Argon^, Natrium^>(Nu->Mu.Value, Rho, Nu->Mu.Key->N->Zeta);
 					Venus->Add(this, Rho);
-					if (Callisto == nullptr) Callisto = gcnew Edge<Nickel^, Argon^, Zinc^>(Nu->Mu.Value, Rho, this);
-					if (Neso == nullptr) Neso = gcnew Edge<Argon^, Zinc^, Aluminium^>(Rho, this, Theta);
+					if (Callisto == nullptr) Callisto = gcnew Edge<Nickel^, Argon^, Sterigma^>(Nu->Mu.Value, Rho, this);
+					if (Neso == nullptr) Neso = gcnew Edge<Argon^, Sterigma^, Aluminium^>(Rho, this, Theta);
 				};
 			};
 		}
