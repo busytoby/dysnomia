c74c118953c1b34189289315bebfce072acb33e9
maria <maria@twee.se>
2023-01-23 10:32:28 -0800

    Belinda As A Dai

--- vs2022/Platform/Elements/24_Ascus.h
+++ vs2022/Platform/Elements/24_Ascus.h
@@ -9,7 +9,7 @@ namespace Dysnomia {
 			public ref class Ascus : public Dai<Synapse^, Eun^> {
 			public:
 				static Edge<Synapse^, Tauon^, Eun^>^ Sycorax;
-				static Edge<Eun^, Dysnomium^, Synapse^>^ Belinda;
+				static Dai<Dysnomium^, Synapse^>^ Belinda;
 				static Edge<Eun^, Sulfur^, Synapse^>^ Puck;
 				static Ascus^ Mars;
 
@@ -18,7 +18,8 @@ namespace Dysnomia {
 
 					if (Mars == nullptr) Mars = this;
 					if (Puck == nullptr) Puck = gcnew Edge<Eun^, Sulfur^, Synapse^>(Iota, Iota[0]->I[0]->I->N, Beta);
-					if (Belinda == nullptr) Belinda = gcnew Edge<Eun^, Dysnomium^, Synapse^>(Iota, Beta->R, Beta);
+					if (Belinda == nullptr) Belinda = gcnew Dai<Dysnomium^, Synapse^>();
+					Belinda->Add(Beta->R, Beta);
 					if (Sycorax == nullptr) Sycorax = gcnew Edge<Synapse^, Tauon^, Eun^>(Beta, Beta->N[0]->L, Iota);
 				}
 			};
