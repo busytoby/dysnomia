63f16147ef21288f118cf7a4bb539ebb14403533
maria <maria@twee.se>
2023-01-22 16:22:32 -0800

    Potassium Renamed Pion

--- vs2022/Platform/Elements/19_Potassium.h
+++ vs2022/Platform/Elements/19_Potassium.h
@@ -5,10 +5,10 @@ using namespace System;
 namespace Dysnomia {
 	namespace Platform {
 		namespace Elements {
-			public ref class Potassium : Edge<Argon^, Dysnomium^, Gluon^>
+			public ref class Pion : Edge<Argon^, Dysnomium^, Gluon^>
 			{
 			public:
-				Potassium(Argon^ Beta) : Edge<Argon^, Dysnomium^, Gluon^>(Beta, gcnew Dysnomium(Beta), Beta->R) {
+				Pion(Argon^ Beta) : Edge<Argon^, Dysnomium^, Gluon^>(Beta, gcnew Dysnomium(Beta), Beta->R) {
 				}
 			};
 		}
--- vs2022/Platform/Elements/20_Ether.h
+++ vs2022/Platform/Elements/20_Ether.h
@@ -5,21 +5,21 @@ using namespace System;
 namespace Dysnomia {
 	namespace Platform {
 		namespace Elements {
-			public ref class Ether : public Dai<Argon^, Potassium^>
+			public ref class Ether : public Dai<Argon^, Pion^>
 			{
 			public:
-				static Edge<Sulfur^, Potassium^, Ether^>^ Deimos;
+				static Edge<Sulfur^, Pion^, Ether^>^ Deimos;
 
 				Ether(Parity^ Beta) {
 					Sulfur^ Rho = gcnew Sulfur(Beta);
 					this->Ether::Ether(Rho);
-					if (Deimos == nullptr) Deimos = gcnew Edge<Sulfur^, Potassium^, Ether^>(Rho, Mu->L, this);
+					if (Deimos == nullptr) Deimos = gcnew Edge<Sulfur^, Pion^, Ether^>(Rho, Mu->L, this);
 				}
 
 				Ether(Sulfur^ Beta) {
 					Chlorine^ Rho = gcnew Chlorine(Beta);
 					Argon^ Sigma = gcnew Argon(Rho);
-					Add(Sigma, gcnew Potassium(Sigma));
+					Add(Sigma, gcnew Pion(Sigma));
 				}
 			};
 		}
