192a3415203d0cbc0f8d9db68cbf4cc80ea05937
maria <maria@twee.se>
2023-01-25 15:56:50 -0800

    Quasar Avoidances Loop For Challenged Bruhs

--- vs2022/Dysnomia/Dysnomia.cpp
+++ vs2022/Dysnomia/Dysnomia.cpp
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include "05_Tin.h"
+#include <iostream>
 
 
 using namespace Dysnomia;
@@ -11,11 +12,22 @@ int main()
     srand((unsigned int)time(NULL));
     Math::Prime = Math::MotzkinPrime;
 
-    Tin* Mu = new Tin();
-    Iron* Rho = new Iron();
-    Qiao* Psi = new Qiao(Rho);
-    Dynamic* Nu = Psi->Tau->Pi();
-    Aluminium* Eta = new Aluminium(Nu, Psi->Tau->Theta->Cone->Base, Psi->Tau->Theta->Rod->Signal, Psi->Tau->Theta->Cone->Signal, Psi->Tau->Theta->Cone->Tau);
-    Iron* Sigma = new Iron(Psi->Eta, Mu->Rho->Eta->Sigma, Psi->Eta->Sigma);
-    Affinity* Upsilon = Sigma->Kappa(Eta);
+    int QuasarAvoidances = 0;
+
+    while (QuasarAvoidances < 10) {
+        try {
+            Tin* Mu = new Tin();
+            Iron* Rho = new Iron();
+            Qiao* Psi = new Qiao(Rho);
+            Dynamic* Nu = Psi->Tau->Pi();
+            Aluminium* Eta = new Aluminium(Nu, Psi->Tau->Theta->Cone->Base, Psi->Tau->Theta->Rod->Signal, Psi->Tau->Theta->Cone->Signal, Psi->Tau->Theta->Cone->Tau);
+            Iron* Sigma = new Iron(Psi->Eta, Mu->Rho->Eta->Sigma, Psi->Eta->Sigma);
+            Affinity* Upsilon = Sigma->Kappa(Eta);
+
+            break;
+        }
+        catch (int e) { QuasarAvoidances++; continue; };
+    }
+
+    cout << "Completed With " << QuasarAvoidances << " Quasar Avoidances\n";
 }
