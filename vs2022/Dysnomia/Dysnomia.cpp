b9b7f2068ee10ab36c46f3a480c3bd05b18a808f
maria <maria@twee.se>
2023-01-28 10:42:42 -0800

    Proof Of The 10th Degree Element 15 Rong

--- vs2022/Dysnomia/Dysnomia.cpp
+++ vs2022/Dysnomia/Dysnomia.cpp
@@ -14,4 +14,9 @@ int main()
     Huo* Mu = new Huo();
     Huo* Rho = new Huo(Mu);
     Xiao* Psi = new Xiao(Rho->Rho);
+    pair<Eun*, Iron*> Four = Psi->Pi();
+    Eun* Nu = Four.first;
+    Iron* Sigma = Four.second;
+    Qiao* Upsilon = new Qiao(Sigma, Sigma->Kappa(Mu->Mu->Mu->Mu->Mu->Mu->Mu->Mu->Mu->Mu));
+    Qiao* Theta = new Qiao(Sigma, Sigma->Kappa(Rho->Mu->Mu->Mu->Mu->Mu->Mu->Mu->Mu->Mu));
 }
