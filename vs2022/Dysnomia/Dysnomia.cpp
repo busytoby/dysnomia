d49f4cc3d58654cbc6c359aa64ce96cbe2d770e3
maria <maria@twee.se>
2023-02-03 12:40:56 -0800

    Proof Of Energy

--- vs2022/Dysnomia/Dysnomia.cpp
+++ vs2022/Dysnomia/Dysnomia.cpp
@@ -17,29 +17,49 @@ int main()
     Qiao* MuBeta = new Qiao(MuAlpha, Alpha->Tau->Mu->Eta->Upsilon->Mu->Upsilon->Beta(), Alpha->Tau->Mu->Eta->Upsilon->Mu->Upsilon->Omicron(), MuAlpha->Mu);
     Tin* MuIota = new Tin(MuBeta->Psi);
     Eun* MuOmicron = new Eun(MuIota, MuAlpha);
+    //
+    Affinity* AlphaMu = Alpha->Tau->Mu->Eta->Upsilon->Mu->Upsilon->Delta(MuOmicron->Mu->Nu, Alpha->Tau->Mu->Eta->Upsilon->Upsilon);
+    //
+    Song* MuLambda = new Song(MuOmicron, AlphaMu);
 
     Iron* RhoAlpha = new Iron();
     Qiao* RhoBeta = new Qiao(RhoAlpha, Alpha->Tau->Mu->Eta->Upsilon->Mu->Upsilon->Beta(), Alpha->Tau->Mu->Eta->Upsilon->Mu->Upsilon->Omicron(), RhoAlpha->Mu);
     Tin* RhoIota = new Tin(RhoBeta->Psi);
     Eun* RhoOmicron = new Eun(RhoIota, RhoAlpha);
+    //
+    Affinity* AlphaRho = Alpha->Tau->Mu->Eta->Upsilon->Mu->Upsilon->Delta(RhoOmicron->Mu->Nu, Alpha->Tau->Mu->Eta->Upsilon->Upsilon);
+    //
+    Song* RhoLambda = new Song(RhoOmicron, AlphaRho);
 
     Iron* PsiAlpha = new Iron();
     Qiao* PsiBeta = new Qiao(PsiAlpha, Alpha->Tau->Mu->Eta->Upsilon->Mu->Upsilon->Beta(), Alpha->Tau->Mu->Eta->Upsilon->Mu->Upsilon->Omicron(), PsiAlpha->Mu);
     Tin* PsiIota = new Tin(PsiBeta->Psi);
     Eun* PsiOmicron = new Eun(PsiIota, PsiAlpha);
+    //
+    Affinity* AlphaPsi = Alpha->Tau->Mu->Eta->Upsilon->Mu->Upsilon->Delta(PsiOmicron->Mu->Nu, Alpha->Tau->Mu->Eta->Upsilon->Upsilon);
+    //
+    Song* PsiLambda = new Song(PsiOmicron, AlphaPsi);
 
     Iron* NuAlpha = new Iron();
     Qiao* NuBeta = new Qiao(NuAlpha, Alpha->Tau->Mu->Eta->Upsilon->Mu->Upsilon->Beta(), Alpha->Tau->Mu->Eta->Upsilon->Mu->Upsilon->Omicron(), NuAlpha->Mu);
     Tin* NuIota = new Tin(NuBeta->Psi);
     Eun* NuOmicron = new Eun(NuIota, NuAlpha);
+    //
+    Affinity* AlphaNu = Alpha->Tau->Mu->Eta->Upsilon->Mu->Upsilon->Delta(NuOmicron->Mu->Nu, Alpha->Tau->Mu->Eta->Upsilon->Upsilon);
+    //
+    Song* NuLambda = new Song(NuOmicron, AlphaNu);
 
     Iron* EtaAlpha = new Iron();
     Qiao* EtaBeta = new Qiao(EtaAlpha, Alpha->Tau->Mu->Eta->Upsilon->Mu->Upsilon->Beta(), Alpha->Tau->Mu->Eta->Upsilon->Mu->Upsilon->Omicron(), EtaAlpha->Mu);
     Tin* EtaIota = new Tin(EtaBeta->Psi);
     Eun* EtaOmicron = new Eun(EtaIota, EtaAlpha);
+    //
+    Affinity* AlphaEta = Alpha->Tau->Mu->Eta->Upsilon->Mu->Upsilon->Delta(EtaOmicron->Mu->Nu, Alpha->Tau->Mu->Eta->Upsilon->Upsilon);
+    //
+    Song* EtaLambda = new Song(EtaOmicron, AlphaEta);
 
-    vector<Affinity*> MuAffinities = MuOmicron->Affinities();
-    vector<Affinity*> RhoAffinities = RhoOmicron->Affinities();
+    vector<Affinity*> MuAffinities = MuLambda->Affinities();
+    vector<Affinity*> RhoAffinities = RhoLambda->Affinities();
 
     for (int i = 0; i < MuAffinities.size(); i++) {
         string A = "MuAffinities["s + to_string(i) + "]";
