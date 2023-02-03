b4ee806714c630b1c6e2ff07d9d7fd5d86f7f85a
maria <maria@twee.se>
2023-02-03 12:24:50 -0800

    Proof Of Cell 365

--- vs2022/Dysnomia/Dysnomia.cpp
+++ vs2022/Dysnomia/Dysnomia.cpp
@@ -15,28 +15,38 @@ int main()
 
     Iron* MuAlpha = new Iron();
     Qiao* MuBeta = new Qiao(MuAlpha, Alpha->Tau->Mu->Eta->Upsilon->Mu->Upsilon->Beta(), Alpha->Tau->Mu->Eta->Upsilon->Mu->Upsilon->Omicron(), MuAlpha->Mu);
+    Tin* MuIota = new Tin(MuBeta->Psi);
+    Eun* MuOmicron = new Eun(MuIota, MuAlpha);
 
     Iron* RhoAlpha = new Iron();
     Qiao* RhoBeta = new Qiao(RhoAlpha, Alpha->Tau->Mu->Eta->Upsilon->Mu->Upsilon->Beta(), Alpha->Tau->Mu->Eta->Upsilon->Mu->Upsilon->Omicron(), RhoAlpha->Mu);
+    Tin* RhoIota = new Tin(RhoBeta->Psi);
+    Eun* RhoOmicron = new Eun(RhoIota, RhoAlpha);
 
     Iron* PsiAlpha = new Iron();
     Qiao* PsiBeta = new Qiao(PsiAlpha, Alpha->Tau->Mu->Eta->Upsilon->Mu->Upsilon->Beta(), Alpha->Tau->Mu->Eta->Upsilon->Mu->Upsilon->Omicron(), PsiAlpha->Mu);
+    Tin* PsiIota = new Tin(PsiBeta->Psi);
+    Eun* PsiOmicron = new Eun(PsiIota, PsiAlpha);
 
     Iron* NuAlpha = new Iron();
     Qiao* NuBeta = new Qiao(NuAlpha, Alpha->Tau->Mu->Eta->Upsilon->Mu->Upsilon->Beta(), Alpha->Tau->Mu->Eta->Upsilon->Mu->Upsilon->Omicron(), NuAlpha->Mu);
+    Tin* NuIota = new Tin(NuBeta->Psi);
+    Eun* NuOmicron = new Eun(NuIota, NuAlpha);
 
     Iron* EtaAlpha = new Iron();
     Qiao* EtaBeta = new Qiao(EtaAlpha, Alpha->Tau->Mu->Eta->Upsilon->Mu->Upsilon->Beta(), Alpha->Tau->Mu->Eta->Upsilon->Mu->Upsilon->Omicron(), EtaAlpha->Mu);
+    Tin* EtaIota = new Tin(EtaBeta->Psi);
+    Eun* EtaOmicron = new Eun(EtaIota, EtaAlpha);
 
-    vector<Affinity*> MuAffinities = MuBeta->Affinities();
-    vector<Affinity*> RhoAffinities = RhoBeta->Affinities();
+    vector<Affinity*> MuAffinities = MuOmicron->Affinities();
+    vector<Affinity*> RhoAffinities = RhoOmicron->Affinities();
 
     for (int i = 0; i < MuAffinities.size(); i++) {
         string A = "MuAffinities["s + to_string(i) + "]";
         MuAffinities[i]->Compare(RhoAffinities[i], A);
     }
 
-    cout << "Set Break Point On Line 41 And Search In Debugger For Matched Pair Values\n";
+    cout << "Set Break Point On Line 49 And Search In Debugger For Matched Pair Values\n";
 
     int i = 99; 
 }
