f81590f9dcf838d2dd9404d7b4923657877592c1
maria <maria@twee.se>
2023-02-03 17:54:52 -0800

    Element 26 Named Fuang, Memory Usage Too High

--- vs2022/Dysnomia/Dysnomia.cpp
+++ vs2022/Dysnomia/Dysnomia.cpp
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <iostream>
-#include "Dong.h"
+#include "26_Fuang.h"
 
 using namespace Dysnomia;
 
@@ -11,22 +11,7 @@ int main()
     srand((unsigned int)time(NULL));
     Math::Prime = Math::MotzkinPrime;
 
-    Dong* Mu = new Dong();
-    Huo* Rho = new Huo(Mu->Theta, Mu->Eta->Tau, Mu->Psi->Theta->Rho, Mu->Upsilon->Rho, Mu->Tau->Rho, Mu->Nu->Theta, Mu->Tau);
-    Rong* Psi = new Rong(Rho, Mu->Sigma->Theta->Rho);
-    Beit* Nu = new Beit(Psi);
-    Wou* Eta = new Wou(Nu);
-    Mu->Mu->Sigma->Alpha();
-    Sing* Sigma = new Sing(Mu->Mu->Sigma, Eta);
-    Eta->Alpha(true);
-    Mu->Mu->Tau->Alpha();
-    Xun* Upsilon = new Xun(Mu->Mu->Tau, Eta);
-    Eta->Alpha(true);
-    Mu->Mu->Sigma->Alpha();
-    Chu* Tau = new Chu(Eta, Mu->Mu->Sigma);
-    Eta->Alpha(true);
-    Mu->Mu->Tau->Alpha();    
-    Chi* Theta = new Chi(Eta, Mu->Mu->Tau);
-
+    Fuang* Mu = new Fuang();
+    
     int i = 99; 
 }
