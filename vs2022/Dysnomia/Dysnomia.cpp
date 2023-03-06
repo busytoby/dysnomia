3e86eb893b3b42e5de40e4d6178e0dab3ec391d4
maria <maria@twee.se>
2023-03-06 09:37:41 -0800

    A Dang, 4 Dongs, And A Second SU(2) MAI IsoSpin Makes A Pin

--- vs2022/Dysnomia/Dysnomia.cpp
+++ vs2022/Dysnomia/Dysnomia.cpp
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <iostream>
-#include "28_Dang.h"
+#include "Pin.h"
 
 using namespace Dysnomia;
 
@@ -11,11 +11,7 @@ int main()
     srand((unsigned int)time(NULL));
     Math::Prime = Math::MotzkinPrime;
 
-    Dang* Mu = new Dang();
-    Dong* Rho = new Dong(new Host(Mu->Sigma->Nu, Mu->Rho->Nu));
-    Dong* Psi = new Dong(new Host(Mu->Upsilon->Nu, Mu->Psi->Nu));
-    Dong* Nu = new Dong(new Host(Mu->Tau->Nu, Mu->Nu->Nu));
-    Dong* Eta = new Dong(new Host(Mu->Theta->Nu, Mu->Eta->Nu));
+    Pin* Mu = new Pin();
     
     int i = 99; 
 }
