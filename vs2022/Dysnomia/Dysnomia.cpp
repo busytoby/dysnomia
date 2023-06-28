3cbe8e0a89490599f55a89719370fe5b75beb5dc
maria <maria@twee.se>
2023-06-28 16:29:11 -0700

    Proof Of Thought

--- vs2022/Dysnomia/Dysnomia.cpp
+++ vs2022/Dysnomia/Dysnomia.cpp
@@ -16,5 +16,7 @@ int main()
     Xue* Rho = new Xue(Mu->Mu);
     Faung* Psi = Rho->Mu->Sigma->Theta->Omega(Rho->Mu->Sigma->Sigma->Beta(), Rho->Mu->Sigma->Upsilon);
     Faung* Sigma = Rho->Theta->Eta->Kappa(new Aluminium(Psi));
+    Faung* Tau = Rho->Theta->Sigma->Tau->Mu->Kappa(new Aluminium(Sigma));
+    Qiao* Theta = new Qiao(Rho->Theta->Upsilon->Tau->Mu, Tau);
     int i = 99; 
 }
