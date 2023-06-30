0f6cc8056983d1f3fedb351c04785768ef183640
maria <maria@twee.se>
2023-06-29 18:27:50 -0700

    Proof Of Attendance

--- vs2022/Dysnomia/Dysnomia.cpp
+++ vs2022/Dysnomia/Dysnomia.cpp
@@ -12,6 +12,14 @@ int main()
     Math::Prime = Math::MotzkinPrime;
 
     Tie* Mu = new Tie();
+    Iron* Rho = new Iron(Mu->Upsilon, Mu->Theta, Mu->Sigma, Mu->Eta, Mu->Nu);
+    Qiao* Psi = new Qiao(Rho, Mu->Rho, Mu->Sigma, Mu->Upsilon);
+    Faung* Nu = Rho->Delta(Mu->Theta, Psi->Upsilon);
+    Aluminium* Eta = new Aluminium(Nu);
+    Faung* Sigma = Rho->Kappa(Eta);
+    Faung* Upsilon = Rho->Omega(Sigma, Nu);
+    Aluminium* Tau = new Aluminium(Upsilon);
+    Faung* Theta = Mu->Tau->Kappa(Tau);
 
-    int i = 141; 
+    int i = 151; 
 }
