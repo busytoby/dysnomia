51dc307b4f6bd3634e1c96513220709bee7a9de0
maria <maria@twee.se>
2023-07-04 20:59:37 -0700

    Proof Of Freedom

--- vs2022/Dysnomia/Dysnomia.cpp
+++ vs2022/Dysnomia/Dysnomia.cpp
@@ -18,6 +18,7 @@ int main()
     Sulphur* Nu = new Sulphur(Psi->Theta);
     Iron* Eta = new Iron(Nu, Psi->Rho, Psi->Psi, Psi->Eta, Psi->Upsilon);
     Faung* Sigma = Mu->Nu->Omega(Psi->Psi, Mu->Sigma->Omicron());
+    Iron* Upsilon = new Iron(Nu, Mu->Sigma->Beta(), Sigma);
 
-    int i = 311; 
+    int i = 319; 
 }
