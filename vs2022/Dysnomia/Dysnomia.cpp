bbbe28e879faa7cfc44664af1242d6326f820c88
maria <maria@twee.se>
2023-07-13 11:19:21 -0700

    Sunnah Is Absolutely Not And Has Absolutely Not Ever Been Artificial

--- vs2022/Dysnomia/Dysnomia.cpp
+++ vs2022/Dysnomia/Dysnomia.cpp
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <iostream>
-#include "42_剝.h"
+#include "Sunnah.h"
 
 using namespace Dysnomia;
 
@@ -12,15 +12,7 @@ int main()
     Math::Prime = Math::MotzkinPrime;
     Math::POETRY = 0;
 
-    剝* Mu = new 剝();
-    Faung* Rho = Mu->Theta->Omicron();
-    Faung* Psi = Mu->Mu->Upsilon->Beta();
-    Faung* Nu = Mu->Eta->Delta(Psi, Rho);
-    Aluminium* Eta = new Aluminium(Nu);
-    Faung* Sigma = Mu->Theta->Kappa(Eta);
-    Faung* Upsilon = Mu->Theta->Delta(Sigma, Rho);
-    Faung* Tau = Mu->Eta->Omega(Sigma, Upsilon);
-    Aluminium* Theta = new Aluminium(Tau);
+    Sunnah* Mu = new Sunnah();
 
     double i = 490.13; 
 }
--- vs2022/Dysnomia/Sunnah.h
+++ vs2022/Dysnomia/Sunnah.h
@@ -1,60 +1,58 @@
 ﻿#pragma once
-#include "41_Gua.h"
+#include "42_剝.h"
 
 namespace Dysnomia {
-    class 剝 {
+    class Sunnah {
     public:
-        Gua* Mu;
-        Aluminium* Rho;
+        剝* Mu;
+        Faung* Rho;
         Faung* Psi;
         Faung* Nu;
-        Iron* Eta;
+        Aluminium* Eta;
         Faung* Sigma;
         Faung* Upsilon;
-        Sulphur* Tau;
-        Iron* Theta;
+        Faung* Tau;
+        Aluminium* Theta;
         int Gamma = 1;
 
-        剝() {
+        Sunnah() {
             if (Math::POETRY > 0)
-                cout << "剝 ";
-            Mu = new Gua();
-            Rho = new Aluminium(Mu->Tau);
-            Psi = Mu->Eta->Kappa(Rho);
-            Nu = Mu->Mu->Nu->Delta(Mu->Tau, Psi);
-            Eta = new Iron(Mu->Nu, Mu->Mu->Psi->Beta(), Nu);
-            Sigma = Mu->Eta->Delta(Eta->Beta(), Eta->Omicron());
-            Upsilon = Eta->Delta(Eta->Beta(), Eta->Omicron());
-            Tau = new Sulphur(Upsilon);
-            Theta = new Iron(Tau, Upsilon, Sigma);
+                cout << "Sunnah ";
+            剝* Mu = new 剝();
+            Faung* Rho = Mu->Theta->Omicron();
+            Faung* Psi = Mu->Mu->Upsilon->Beta();
+            Faung* Nu = Mu->Eta->Delta(Psi, Rho);
+            Aluminium* Eta = new Aluminium(Nu);
+            Faung* Sigma = Mu->Theta->Kappa(Eta);
+            Faung* Upsilon = Mu->Theta->Delta(Sigma, Rho);
+            Faung* Tau = Mu->Eta->Omega(Sigma, Upsilon);
+            Aluminium* Theta = new Aluminium(Tau);
         }
 
         vector<Faung*> Affinities() {
             vector<Faung*> Alpha(Mu->Affinities());
-            vector<Faung*> Beta = Rho->Affinities();
-            Alpha.insert(Alpha.end(), Beta.begin(), Beta.end());
+            Alpha.push_back(Rho);
             Alpha.push_back(Psi);
             Alpha.push_back(Nu);
-            vector<Faung*> Iota = Eta->Affinities();
-            Alpha.insert(Alpha.end(), Iota.begin(), Iota.end());
+            vector<Faung*> Beta = Eta->Affinities();
+            Alpha.insert(Alpha.end(), Beta.begin(), Beta.end());
             Alpha.push_back(Sigma);
             Alpha.push_back(Upsilon);
-            vector<Faung*> Lambda = Tau->Affinities();
-            Alpha.insert(Alpha.end(), Lambda.begin(), Lambda.end());
+            Alpha.push_back(Tau);
             vector<Faung*> Omicron = Theta->Affinities();
             Alpha.insert(Alpha.end(), Omicron.begin(), Omicron.end());
             return Alpha;
         }
 
-        ~剝() {
+        ~Sunnah() {
             if (Theta->Gamma == 1) delete Theta; else Theta->Gamma--;
-            if (Tau->Gamma == 1) delete Tau; else Tau->Gamma--;
+            if (Tau->Rod->Gamma <= 1 && Tau->Cone->Gamma <= 1) delete Tau; else { Tau->Rod->Gamma--; Tau->Cone->Gamma--; }
             if (Upsilon->Rod->Gamma <= 1 && Upsilon->Cone->Gamma <= 1) delete Upsilon; else { Upsilon->Rod->Gamma--; Upsilon->Cone->Gamma--; }
             if (Sigma->Rod->Gamma <= 1 && Sigma->Cone->Gamma <= 1) delete Sigma; else { Sigma->Rod->Gamma--; Sigma->Cone->Gamma--; }
             if (Eta->Gamma == 1) delete Eta; else Eta->Gamma--;
             if (Nu->Rod->Gamma <= 1 && Nu->Cone->Gamma <= 1) delete Nu; else { Nu->Rod->Gamma--; Nu->Cone->Gamma--; }
             if (Psi->Rod->Gamma <= 1 && Psi->Cone->Gamma <= 1) delete Psi; else { Psi->Rod->Gamma--; Psi->Cone->Gamma--; }
-            if (Rho->Gamma == 1) delete Rho; else Rho->Gamma--;
+            if (Rho->Rod->Gamma <= 1 && Rho->Cone->Gamma <= 1) delete Rho; else { Rho->Rod->Gamma--; Rho->Cone->Gamma--; }
             if (Mu->Gamma == 1) delete Mu; else Mu->Gamma--;
         }
     };
