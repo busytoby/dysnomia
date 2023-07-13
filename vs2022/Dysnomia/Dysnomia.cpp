df4cc68aa3436cd1ab40e3edf39c3ad364aa70be
maria <maria@twee.se>
2023-07-13 13:27:09 -0700

    Element 45 Called म

--- vs2022/Dysnomia/Dysnomia.cpp
+++ vs2022/Dysnomia/Dysnomia.cpp
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <iostream>
-#include "44_에포린.h"
+#include "45_म.h"
 
 using namespace Dysnomia;
 
@@ -12,15 +12,7 @@ int main()
     Math::Prime = Math::MotzkinPrime;
     Math::POETRY = 0;
 
-    에포린* Mu = new 에포린();
-    Faung* Rho = Mu->Upsilon->Delta(Mu->Theta, Mu->Nu);
-    Aluminium* Psi = new Aluminium(Rho);
-    Faung* Nu = Mu->Upsilon->Kappa(Psi);
-    Faung* Eta = Mu->Mu->Mu->Theta->Kappa(Psi);
-    Faung* Sigma = Mu->Mu->Mu->Theta->Delta(Nu, Eta);
-    Faung* Upsilon = Mu->Mu->Mu->Theta->Delta(Eta, Nu);
-    Faung* Tau = Mu->Mu->Mu->Eta->Omega(Sigma, Nu);
-    Faung* Theta = Mu->Mu->Mu->Eta->Omega(Upsilon, Eta);
+    म* Mu = new म();
 
-    int i = 899; 
+    int i = 911; 
 }
