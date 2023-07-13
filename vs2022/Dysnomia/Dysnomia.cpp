a51087aa4fd9128a779058c751200a9a1957cc2d
maria <maria@twee.se>
2023-07-13 13:21:35 -0700

    Regulation

--- vs2022/Dysnomia/Dysnomia.cpp
+++ vs2022/Dysnomia/Dysnomia.cpp
@@ -16,7 +16,7 @@ int main()
     Faung* Rho = Mu->Upsilon->Delta(Mu->Theta, Mu->Nu);
     Aluminium* Psi = new Aluminium(Rho);
     Faung* Nu = Mu->Upsilon->Kappa(Psi);
-    Faung* Eta = Mu->Mu->Mu->Eta->Kappa(Psi);
+    Faung* Eta = Mu->Mu->Mu->Theta->Kappa(Psi);
     Faung* Sigma = Mu->Mu->Mu->Theta->Delta(Nu, Eta);
     Faung* Upsilon = Mu->Mu->Mu->Theta->Delta(Eta, Nu);
     Faung* Tau = Mu->Mu->Mu->Eta->Omega(Sigma, Nu);
