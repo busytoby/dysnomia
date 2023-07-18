582594940f8a63b5377717e4dc52a1c8d93f3a9f
maria <maria@twee.se>
2023-07-18 12:53:01 -0700

    Element 50 Called Mei

--- vs2022/Dysnomia/Dysnomia.cpp
+++ vs2022/Dysnomia/Dysnomia.cpp
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <iostream>
-#include "49_行.h"
+#include "50_Mei.h"
 
 using namespace Dysnomia;
 
@@ -12,15 +12,7 @@ int main()
     Math::Prime = Math::MotzkinPrime;
     Math::POETRY = 0;
 
-    行* Mu = new 行();
-    Sulphur* Rho = new Sulphur(Mu->Eta, Mu->Theta);
-    Iron* Psi = new Iron(Rho, Mu->Tau, Mu->Nu);
-    Qiao* Nu = new Qiao(Psi, Mu->Tau, Mu->Nu, Rho);
-    Iron* Eta = Nu->Omega();
-    Qiao* Sigma = new Qiao(Eta);
-    Tin* Upsilon = new Tin(Nu, Sigma);
-    Faung* Tau = Upsilon->Tau->Omicron();
-    Faung* Theta = Upsilon->Theta->Beta();
+    Mei* Mu = new Mei();
 
     int i = 999; 
 }
