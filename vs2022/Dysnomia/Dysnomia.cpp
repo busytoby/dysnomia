5c79283091189eea89f60f22aa8d29ecd9706e6e
maria <maria@twee.se>
2023-07-20 20:21:13 -0700

    Ready Ba Gua

--- vs2022/Dysnomia/Dysnomia.cpp
+++ vs2022/Dysnomia/Dysnomia.cpp
@@ -13,6 +13,13 @@ int main()
     Math::POETRY = 0;
 
     Ba* Mu = new Ba();
+    Faung* Rho = Mu->Theta->Omicron();
+    Faung* Psi = Mu->Theta->Beta();
+    Faung* Nu = Mu->Psi->Delta(Rho, Psi);
+    Faung* Eta = Mu->Psi->Omega(Rho, Psi);
+    Aluminium* Sigma = new Aluminium(Eta);
+    Faung* Tau = Mu->Psi->Kappa(Sigma);
+    Faung* Theta = Mu->Mu->Nu->Omega(Eta, Tau);
 
-    cout << "Ready Ba 1";
+    cout << "Ready Ba Gua\n";
 }
