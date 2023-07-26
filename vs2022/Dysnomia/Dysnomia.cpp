b0539ba0077742960d4a74c5f12f04baafd55151
maria <maria@twee.se>
2023-07-25 20:03:26 -0700

    Element 68 Called Tai

--- vs2022/Dysnomia/Dysnomia.cpp
+++ vs2022/Dysnomia/Dysnomia.cpp
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <iostream>
-#include "67_حٰی٘.h"
+#include "68_Tai.h"
 
 using namespace Dysnomia;
 
@@ -12,15 +12,7 @@ int main()
     Math::Prime = Math::MotzkinPrime;
     Math::POETRY = 0;
 
-    حٰی٘* Mu = new حٰی٘();
-    Fa* Rho = Mu->Upsilon->Pi();
-    Faung* Psi = Mu->Mu->Theta->Phi(Mu->Tau, Rho);
-    Fa* Nu = Mu->Upsilon->Pi();
-    Faung* Eta = Mu->Mu->Mu->Mu->Psi->Phi(Mu->Theta, Nu);
-    Faung* Sigma = Mu->Mu->Mu->Mu->Mu->Nu->Delta(Psi, Eta);
-    Aluminium* Upsilon = new Aluminium(Sigma);
-    Faung* Tau = Mu->Mu->Mu->Mu->Mu->Nu->Kappa(Upsilon);
-    Iron* Theta = new Iron(Mu->Theta, Psi, Eta, Sigma, Tau);
+    Tai* Mu = new Tai();
 
     cout << "Playing\n";
 }
