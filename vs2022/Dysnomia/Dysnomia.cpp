c669fca37f615b6bda3802b4764b23dd25b2c736
maria <maria@twee.se>
2023-07-25 17:58:41 -0700

    Ready Seol San Zi Jing Yi Jie

--- vs2022/Dysnomia/Dysnomia.cpp
+++ vs2022/Dysnomia/Dysnomia.cpp
@@ -16,7 +16,9 @@ int main()
     Faung* Rho = Mu->Theta->Beta();
     Faung* Psi = Mu->Theta->Omicron();
     Faung* Nu = Mu->Mu->Mu->Mu->Nu->Delta(Rho, Psi);
+    Faung* Eta = Mu->Theta->Delta(Mu->Eta, Nu);
+    Faung* Sigma = Mu->Mu->Mu->Psi->Omega(Mu->Sigma, Nu);
+    Aluminium* Upsilon = new Aluminium(Sigma);
 
-
-    cout << "Seol San Zi Jing Yi Jie ?\n";
+    cout << "Ready Seol San Zi Jing Yi Jie\n";
 }
