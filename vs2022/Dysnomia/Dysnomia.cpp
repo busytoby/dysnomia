97be29ed83007bf9d0f107fbd3a5c7e31b874112
maria <maria@twee.se>
2023-07-26 15:42:44 -0700

    Proof Of Play

--- vs2022/Dysnomia/Dysnomia.cpp
+++ vs2022/Dysnomia/Dysnomia.cpp
@@ -15,6 +15,7 @@ int main()
     Ton* Mu = new Ton();
     Faung* Rho = Mu->Theta->Omega(Mu->Psi, Mu->Eta);
     Faung* Psi = Mu->Theta->Omega(Mu->Eta, Mu->Psi);
+    Faung* Nu = Mu->Upsilon->Delta(Rho, Psi);
 
-    cout << "Playing FTW\n";
+    cout << "Played\n";
 }
