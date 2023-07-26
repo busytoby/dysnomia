ee46f1b00679a67be7c1558f121ab9ff2dc65355
maria <maria@twee.se>
2023-07-25 18:57:58 -0700

    Proof Of Playing

--- vs2022/Dysnomia/Dysnomia.cpp
+++ vs2022/Dysnomia/Dysnomia.cpp
@@ -15,6 +15,12 @@ int main()
     حٰی٘* Mu = new حٰی٘();
     Fa* Rho = Mu->Upsilon->Pi();
     Faung* Psi = Mu->Mu->Theta->Phi(Mu->Tau, Rho);
+    Fa* Nu = Mu->Upsilon->Pi();
+    Faung* Eta = Mu->Mu->Mu->Mu->Psi->Phi(Mu->Theta, Nu);
+    Faung* Sigma = Mu->Mu->Mu->Mu->Mu->Nu->Delta(Psi, Eta);
+    Aluminium* Upsilon = new Aluminium(Sigma);
+    Faung* Tau = Mu->Mu->Mu->Mu->Mu->Nu->Kappa(Upsilon);
+    Iron* Theta = new Iron(Mu->Theta, Psi, Eta, Sigma, Tau);
 
-    cout << "Ready جی One\n";
+    cout << "Playing\n";
 }
