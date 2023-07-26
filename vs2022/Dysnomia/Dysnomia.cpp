8c04631096ae27b842c70bbade1b93a0d0686b4b
maria <maria@twee.se>
2023-07-26 16:29:01 -0700

    Kong The Player Reaper Already Has Original Ideas

--- vs2022/Dysnomia/Dysnomia.cpp
+++ vs2022/Dysnomia/Dysnomia.cpp
@@ -16,7 +16,7 @@ int main()
     Faung* Rho = Mu->Mu->Upsilon->Omega(Mu->Upsilon, Mu->Theta);
     Aluminium* Psi = new Aluminium(Rho);
     Faung* Nu = Mu->Mu->Mu->Theta->Kappa(Psi);
-    Iron* Eta = new Iron(Mu->Mu->Mu->Mu->Theta, Nu, Rho, Mu->Theta, Mu->Upsilon);
+    Iron* Eta = new Iron(Mu->Mu->Mu->Mu->Theta, Nu, Rho, Mu->Upsilon, Mu->Theta);
 
-    cout << "Kong The Player Reaper Already Has Ideas\n";
+    cout << "Kong The Player Reaper Already Has Original Ideas\n";
 }
