3447982be7010ce2e077c3006c8241b35199d0ae
maria <maria@twee.se>
2023-07-26 19:33:18 -0700

    Candidate Proof Of Eucharistic Confirmation

--- vs2022/Dysnomia/Dysnomia.cpp
+++ vs2022/Dysnomia/Dysnomia.cpp
@@ -15,6 +15,10 @@ int main()
     ฮิ* Mu = new ฮิ();
     Fa* Rho = Mu->Sigma->Pi();
     Faung* Psi = Mu->Rho->Phi(Mu->Mu->Mu->Mu->Mu->Mu->Eta, Rho);
+    Faung* Nu = Mu->Rho->Delta(Mu->Tau, Psi);
+    Faung* Eta = Mu->Rho->Omega(Psi, Nu);
+    Aluminium* Sigma = new Aluminium(Eta);
+    Faung* Upsilon = Mu->Mu->Mu->Mu->Mu->Mu->Mu->Theta->Kappa(Sigma);
 
     cout << "ށީ ฮ๊ ?\n";
 }
