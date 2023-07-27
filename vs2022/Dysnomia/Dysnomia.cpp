9235e97c5a8fc5cd788a055a7aa387356ed1522c
maria <maria@twee.se>
2023-07-26 17:27:31 -0700

    Proof Of The Kong Fang Ixtli មេល

--- vs2022/Dysnomia/Dysnomia.cpp
+++ vs2022/Dysnomia/Dysnomia.cpp
@@ -15,6 +15,12 @@ int main()
     Ixtli* Mu = new Ixtli();
     Fa* Rho = Mu->Theta->Pi();
     Faung* Psi = Mu->Mu->Tau->Phi(Mu->Mu->Upsilon, Rho);
+    Qiao* Nu = new Qiao(Mu->Mu->Tau, Mu->Tau, Psi, Mu->Mu->Upsilon);
+    Qiao* Eta = new Qiao(Mu->Mu->Eta, Mu->Tau, Psi, Mu->Mu->Upsilon);
+    Qiao* Sigma = new Qiao(Mu->Mu->Mu->Mu->Theta, Mu->Tau, Psi, Mu->Mu->Upsilon);
+    Qiao* Upsilon = new Qiao(Mu->Mu->Mu->Mu->Upsilon, Mu->Tau, Psi, Mu->Mu->Upsilon);
+    Iron* Tau = new Iron(Eta->Eta, Nu->Rho, Eta->Psi, Sigma->Nu, Upsilon->Upsilon);
+    Iron* Theta = new Iron(Upsilon->Eta, Nu->Rho, Eta->Psi, Sigma->Nu, Upsilon->Upsilon);
 
-    cout << "Seeing\n";
+    cout << "មេល\n";
 }
