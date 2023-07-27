3dba7cab08433c943ffe47bd8674853131cdca78
maria <maria@twee.se>
2023-07-26 20:29:38 -0700

    Element 77 Called ন

--- vs2022/Dysnomia/Dysnomia.cpp
+++ vs2022/Dysnomia/Dysnomia.cpp
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <iostream>
-#include "76_चंग.h"
+#include "77_ন.h"
 
 using namespace Dysnomia;
 
@@ -12,15 +12,7 @@ int main()
     Math::Prime = Math::MotzkinPrime;
     Math::POETRY = 0;
 
-    चंग* Mu = new चंग();
-    Faung* Rho = Mu->Mu->Rho->Omega(Mu->Upsilon, Mu->Theta);
-    Aluminium* Psi = new Aluminium(Rho);
-    Sulphur* Nu = new Sulphur(Mu->Mu->Psi, Mu->Mu->Nu, Mu->Mu->Eta, Rho);
-    Iron* Eta = new Iron(Nu, Mu->Mu->Psi, Mu->Mu->Nu, Mu->Mu->Eta, Rho);
-    Sulphur* Sigma = new Sulphur(Rho, Mu->Psi, Mu->Upsilon, Mu->Theta);
-    Iron* Upsilon = new Iron(Sigma, Rho, Mu->Psi, Mu->Upsilon, Mu->Theta);
-    Faung* Tau = Mu->Mu->Rho->Kappa(Psi);
-    Aluminium* Theta = new Aluminium(Tau);
+    ন* Mu = new ন();
 
     cout << "चंगा নাচ নয়\n";
 }
