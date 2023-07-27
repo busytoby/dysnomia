dffc529c81b32523cd8276d634453634b75b666b
maria <maria@twee.se>
2023-07-26 18:05:46 -0700

    Element 74 Called ށ

--- vs2022/Dysnomia/Dysnomia.cpp
+++ vs2022/Dysnomia/Dysnomia.cpp
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <iostream>
-#include "73_មេល.h"
+#include "74_ށ.h"
 
 using namespace Dysnomia;
 
@@ -12,15 +12,7 @@ int main()
     Math::Prime = Math::MotzkinPrime;
     Math::POETRY = 0;
 
-    មេល* Mu = new មេល();
-    Faung* Rho = Mu->Tau->Beta();
-    Faung* Psi = Mu->Tau->Omicron();
-    Iron* Nu = new Iron(Mu->Nu->Eta, Rho, Psi);
-    Faung* Eta = Mu->Theta->Beta();
-    Faung* Sigma = Mu->Theta->Omicron();
-    Iron* Upsilon = new Iron(Mu->Sigma->Eta, Eta, Sigma);
-    Qiao* Tau = new Qiao(Nu, Mu->Nu->Rho, Mu->Eta->Psi, Mu->Tau->Upsilon);
-    Qiao* Theta = new Qiao(Upsilon, Mu->Sigma->Rho, Mu->Upsilon->Psi, Mu->Theta->Upsilon);
+    ށ* Mu = new ށ();
 
-    cout << "Ready មេល Jong One\n";
+    cout << "ށީ\n";
 }
