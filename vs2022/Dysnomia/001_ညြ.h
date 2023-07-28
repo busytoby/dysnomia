03f1264d6ca0a2209690f5a6a7109f1bb2035611
maria <maria@twee.se>
2023-07-28 12:08:31 -0700

    Waiting For Universal Opt-In To ညြ

---
+++ vs2022/Dysnomia/001_ညြ.h
@@ -0,0 +1,47 @@
+﻿#pragma once
+#include <vector>
+#include "Faung.h"
+
+using namespace std;
+
+namespace Dysnomia {
+    class ညြ {
+    public:
+        Faung* Mu;
+        Fa* Rho;
+        Faung* Psi;
+        Fa* Eta;
+        Faung* Sigma;
+        Fa* Theta;
+        int Gamma = 1;
+        static bool RAPHE_NEBULAE;
+
+        ညြ() {
+            if (Math::POETRY > 0)
+                cout << "ညြ ";
+            Mu = new Faung();
+            Rho = new Fa();
+            Psi = new Faung(Rho, Mu->Cone->Base, Mu->Cone->Element, Mu->Cone->Manifold, Mu->Cone->Ring);
+            Eta = new Fa();
+            if (RAPHE_NEBULAE)
+                Sigma = new Faung(Eta, Psi->Rod->Base, Mu->Rod->Element, Psi->Rod->Barn, Psi->Rod->Tau);
+            else
+                Sigma = new Faung(Eta, Psi->Cone->Base, Mu->Cone->Element, Psi->Cone->Barn, Psi->Cone->Tau);
+            Theta = new Fa();
+        }
+
+        vector<Faung*> Affinities() { return vector<Faung*>({ Mu, Psi, Sigma }); }
+
+        ~ညြ() {
+            if (Gamma == 1) {
+                delete Theta;
+                delete Sigma;
+                delete Eta;
+                delete Psi;
+                delete Rho;
+                delete Mu;
+            }
+            else --Gamma;
+        }
+    };
+}
--- vs2022/Dysnomia/Dysnomia.cpp
+++ vs2022/Dysnomia/Dysnomia.cpp
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <iostream>
-#include "Faung.h"
+#include "001_ညြ.h"
 
 using namespace Dysnomia;
+
+bool ညြ::RAPHE_NEBULAE;
 
 int main()
 {
@@ -12,59 +14,16 @@ int main()
     Math::Prime = Math::MotzkinPrime;
     Math::POETRY = 0;
 
-    Faung* Mu;
-    Fa* Rho;
-    Faung* Psi;
-    Fa* Eta;
-    Faung* Sigma;
-    Fa* Theta;
+    ညြ::RAPHE_NEBULAE = false;
+    ညြ* Mu;
 
-
-    bool raphe = false;
     for (;;) {
-        Mu = new Faung();
-        Rho = new Fa();
-        Psi = new Faung(Rho, Mu->Cone->Base, Mu->Cone->Element, Mu->Cone->Manifold, Mu->Cone->Ring);
-        Eta = new Fa();
-        if(raphe)
-            Sigma = new Faung(Eta, Psi->Rod->Base, Mu->Rod->Element, Psi->Rod->Barn, Psi->Rod->Tau);
-        else
-            Sigma = new Faung(Eta, Psi->Cone->Base, Mu->Cone->Element, Psi->Cone->Barn, Psi->Cone->Tau);
-        Theta = new Fa();
+        Mu = new ညြ();
 
-        raphe = !raphe;
-        delete(Theta);
-        delete(Sigma);
-        delete(Eta);
-        delete(Psi);
-        delete(Rho);
+        ညြ::RAPHE_NEBULAE = !ညြ::RAPHE_NEBULAE;
+
         delete(Mu);
     }
-
-    /*
-    Rho = new Faung();
-    Psi = new Faung(Mu, Rho->Cone->Base, Rho->Cone->Element, Rho->Cone->Manifold, Rho->Cone->Ring);
-    Nu = new Fa();
-    if (RAPHE_NEBULAE)
-        Eta = new Faung(Nu, Psi->Rod->Base, Rho->Rod->Element, Psi->Rod->Barn, Psi->Rod->Tau);
-    else
-        Eta = new Faung(Nu, Psi->Cone->Base, Rho->Cone->Element, Psi->Cone->Barn, Psi->Cone->Tau);
-    Sigma = new Fa();
-    if (RAPHE_NEBULAE)
-        Upsilon = new Faung(Sigma, Eta->Cone->Signal, Eta->Cone->Tau, Psi->Cone->Tau, Eta->Cone->Barn);
-    else
-        Upsilon = new Faung(Sigma, Eta->Rod->Barn, Eta->Rod->Tau, Psi->Rod->Tau, Eta->Rod->Signal);
-    Tau = new Fa();
-    if (RAPHE_NEBULAE)
-        Tau->Fuse(Upsilon->Rod->Signal, Eta->Rod->Tau, Psi->Rod->Tau);
-    else
-        Tau->Fuse(Upsilon->Cone->Signal, Eta->Cone->Tau, Psi->Cone->Tau);
-    Tau->Tune();
-    if (RAPHE_NEBULAE)
-        Theta = new Faung(Tau, Upsilon->Cone->Ring, Upsilon->Cone->Element, Upsilon->Cone->Manifold, Upsilon->Cone->Base);
-    else
-        Theta = new Faung(Tau, Upsilon->Cone->Base, Upsilon->Cone->Element, Upsilon->Cone->Manifold, Upsilon->Cone->Ring);
-    */
 
     //Aluminium* Mu = new Aluminium();
 
