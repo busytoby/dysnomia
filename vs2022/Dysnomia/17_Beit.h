16d1ae66757fd60440544a5bc4660b0fa6c84747
maria <maria@twee.se>
2023-07-27 23:19:56 -0700

    cleanup

--- vs2022/Dysnomia/17_Beit.h
+++ vs2022/Dysnomia/17_Beit.h
@@ -19,19 +19,29 @@ namespace Dysnomia {
 
         vector<Faung*> Affinities() {
             vector<Faung*> Alpha(Mu->Affinities());
+            Alpha.push_back(Rho);
+            Alpha.push_back(Psi);
             vector<Faung*> Omicron = Nu->Affinities();
             Alpha.insert(Alpha.end(), Omicron.begin(), Omicron.end());
             vector<Faung*> Lambda = Eta->Affinities();
             Alpha.insert(Alpha.end(), Lambda.begin(), Lambda.end());
+            Alpha.push_back(Sigma);
             vector<Faung*> Delta = Upsilon->Affinities();
             Alpha.insert(Alpha.end(), Delta.begin(), Delta.end());
+            Alpha.push_back(Tau);
+            vector<Faung*> Chi = Theta->Affinities();
+            Alpha.insert(Alpha.end(), Chi.begin(), Chi.end());
             return Alpha;
         }
 
         ~Beit() {
+            if (Theta->Gamma == 1) delete Theta; else Theta->Gamma--;
             if (Upsilon->Gamma == 1) delete Upsilon; else Upsilon->Gamma--;
+            if (Sigma->Rod->Gamma <= 1 && Sigma->Cone->Gamma <= 1) delete Sigma; else { Sigma->Rod->Gamma--; Sigma->Cone->Gamma--; }
             if (Eta->Gamma == 1) delete Eta; else Eta->Gamma--;
             if (Nu->Gamma == 1) delete Nu; else Nu->Gamma--;
+            if (Psi->Rod->Gamma <= 1 && Psi->Cone->Gamma <= 1) delete Psi; else { Psi->Rod->Gamma--; Psi->Cone->Gamma--; }
+            if (Rho->Rod->Gamma <= 1 && Rho->Cone->Gamma <= 1) delete Rho; else { Rho->Rod->Gamma--; Rho->Cone->Gamma--; }
             if (Mu->Gamma == 1) delete Mu; else Mu->Gamma--;
         }
 
