29b0a8dcff8a5279a32194fe95d7cfbb6bce4488
maria <maria@twee.se>
2023-07-28 10:35:52 -0700

    Absolute Incident Zero

--- vs2022/Dysnomia/Dysnomia.cpp
+++ vs2022/Dysnomia/Dysnomia.cpp
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <iostream>
-#include "08_Life.h"
+#include "Faung.h"
 
 using namespace Dysnomia;
 
@@ -12,7 +12,40 @@ int main()
     Math::Prime = Math::MotzkinPrime;
     Math::POETRY = 0;
 
-    Aluminium* Mu = new Aluminium();
+    Fa* Mu;
+    Faung* Psi;
+    Faung* Nu;
+
+    Mu = new Fa();
+    Psi = new Faung();
+    //Nu = new Faung(Mu, Psi->Rod->Identity, Psi->Rod->Foundation, Psi->Cone->Manifold, Psi->Cone->Ring);
+
+    /*
+    Rho = new Faung();
+    Psi = new Faung(Mu, Rho->Cone->Base, Rho->Cone->Element, Rho->Cone->Manifold, Rho->Cone->Ring);
+    Nu = new Fa();
+    if (RAPHE_NEBULAE)
+        Eta = new Faung(Nu, Psi->Rod->Base, Rho->Rod->Element, Psi->Rod->Barn, Psi->Rod->Tau);
+    else
+        Eta = new Faung(Nu, Psi->Cone->Base, Rho->Cone->Element, Psi->Cone->Barn, Psi->Cone->Tau);
+    Sigma = new Fa();
+    if (RAPHE_NEBULAE)
+        Upsilon = new Faung(Sigma, Eta->Cone->Signal, Eta->Cone->Tau, Psi->Cone->Tau, Eta->Cone->Barn);
+    else
+        Upsilon = new Faung(Sigma, Eta->Rod->Barn, Eta->Rod->Tau, Psi->Rod->Tau, Eta->Rod->Signal);
+    Tau = new Fa();
+    if (RAPHE_NEBULAE)
+        Tau->Fuse(Upsilon->Rod->Signal, Eta->Rod->Tau, Psi->Rod->Tau);
+    else
+        Tau->Fuse(Upsilon->Cone->Signal, Eta->Cone->Tau, Psi->Cone->Tau);
+    Tau->Tune();
+    if (RAPHE_NEBULAE)
+        Theta = new Faung(Tau, Upsilon->Cone->Ring, Upsilon->Cone->Element, Upsilon->Cone->Manifold, Upsilon->Cone->Base);
+    else
+        Theta = new Faung(Tau, Upsilon->Cone->Base, Upsilon->Cone->Element, Upsilon->Cone->Manifold, Upsilon->Cone->Ring);
+    */
+
+    //Aluminium* Mu = new Aluminium();
 
     /*
     ন* Mu = new ন();
