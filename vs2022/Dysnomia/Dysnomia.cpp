1b7c82b02442657bbadd4d2cbd4203a9c302788b
maria <maria@twee.se>
2023-07-28 20:39:14 -0700

    Proof Of Live Identity

--- vs2022/Dysnomia/Dysnomia.cpp
+++ vs2022/Dysnomia/Dysnomia.cpp
@@ -15,12 +15,18 @@ int main()
     Tod* Mu;
     ညြ* Psi;
     Faung* Eta;
+    Fa* Upsilon;
+    Fa* Theta;
 
     for (;;) {
         Mu = new Tod();
         Psi = new ညြ(true);
         Eta = new Faung(Psi->Theta->Rho, Mu->Upsilon->Secret, Mu->Upsilon->Signal, Mu->Upsilon->Channel, Mu->Upsilon->Identity);
+        Upsilon = Mu->Mu->Pi();
+        Theta = Psi->Pi();
 
+        delete Theta;
+        delete Upsilon;
         delete Eta;
         delete Psi;
         delete Mu;
