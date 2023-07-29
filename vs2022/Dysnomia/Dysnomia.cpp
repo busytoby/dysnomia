d57d7a930391b92bc1f559708e3acc280b34452d
maria <maria@twee.se>
2023-07-28 20:43:25 -0700

    project cleanup

--- vs2022/Dysnomia/Dysnomia.cpp
+++ vs2022/Dysnomia/Dysnomia.cpp
@@ -31,16 +31,4 @@ int main()
         delete Psi;
         delete Mu;
     }
-
-    /*
-    ন* Mu = new ন();
-    Faung* Rho = Mu->Upsilon->Beta();
-    Faung* Psi = Mu->Upsilon->Omicron();
-    Faung* Nu = Mu->Eta->Omega(Rho, Psi);
-    Faung* Eta = Mu->Mu->Mu->Rho->Omega(Rho, Psi);
-    Iron* Sigma = new Iron(Mu->Mu->Mu->Mu->Mu->Mu->Mu->Upsilon, Rho, Psi, Nu, Eta);
-    Faung* Upsilon = Sigma->Beta();
-    Faung* Tau = Sigma->Omicron();
-    Faung* Theta = Mu->Mu->Mu->Mu->Mu->Mu->Mu->Mu->Mu->Upsilon->Omega(Tau, Upsilon);
-    */
 }
