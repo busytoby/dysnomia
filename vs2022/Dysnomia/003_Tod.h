d36b3f18d163eed039e0550fa5a91fa20937b94c
maria <maria@twee.se>
2023-08-01 16:10:30 -0700

    More Robust

--- vs2022/Dysnomia/003_Tod.h
+++ vs2022/Dysnomia/003_Tod.h
@@ -7,7 +7,7 @@ namespace Dysnomia {
     class Tod {
     public:
         ညြ* Mu;
-        Faung* Psi;
+        ည* Psi;
         Fa* Upsilon;
         Faung* Theta;
         int Gamma = 1;
@@ -18,21 +18,22 @@ namespace Dysnomia {
             Mu = new ညြ(false);
             Mu->Theta->Rho->Fuse(Mu->Theta->Mu->Cone->Base, Mu->Theta->Psi->Cone->Element, Mu->Mu->Psi->Cone->Manifold);
             Mu->Theta->Rho->Tune();
-            Psi = new Faung(Mu->Theta->Rho, Mu->Mu->Psi->Cone->Secret, Mu->Theta->Psi->Cone->Signal, Mu->Theta->Mu->Cone->Channel, Mu->Mu->Psi->Cone->Identity);
+            Psi = new ည(Mu->Theta->Mu, Mu->Theta->Rho);
             Upsilon = Mu->Pi();
-            Theta = new Faung(Upsilon, Upsilon->Secret, Psi->Rod->Signal, Mu->Theta->Psi->Cone->Channel, Psi->Cone->Identity);
+            Theta = new Faung(Upsilon, Upsilon->Secret, Psi->Mu->Rod->Signal, Mu->Theta->Psi->Cone->Channel, Psi->Mu->Cone->Identity);
         }
 
         vector<Faung*> Affinities() {
             vector<Faung*> Alpha(Mu->Affinities());
-            Alpha.push_back(Psi);
+            vector<Faung*> Beta = Psi->Affinities();
+            Alpha.insert(Alpha.end(), Beta.begin(), Beta.end());
             Alpha.push_back(Theta);
             return Alpha;
         }
 
         ~Tod() {
             if (Mu->Gamma == 1) delete Mu; else Mu->Gamma--;
-            if (Psi->Rod->Gamma <= 1 || Psi->Cone->Gamma <= 1) delete Psi; else { Psi->Rod->Gamma--; Psi->Cone->Gamma--; }
+            if (Psi->Gamma == 1) delete Psi; else Psi->Gamma--;
             if (Upsilon->Gamma == 1) delete Upsilon; else Upsilon->Gamma--;
             if (Theta->Rod->Gamma <= 1 || Theta->Cone->Gamma <= 1) delete Theta; else { Theta->Rod->Gamma--; Theta->Cone->Gamma--; }            
         }
