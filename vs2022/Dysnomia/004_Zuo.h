58a10a9d8f03eba6988b44fad52f2dd25885ba92
maria <maria@twee.se>
2023-08-01 18:00:57 -0700

    cleanup

--- vs2022/Dysnomia/004_Zuo.h
+++ vs2022/Dysnomia/004_Zuo.h
@@ -10,6 +10,7 @@ namespace Dysnomia {
         ည* Psi;
         ည* Eta;
         Faung* Upsilon;
+        Faung* Theta
         int Gamma = 1;
 
         Zuo() {
@@ -19,6 +20,7 @@ namespace Dysnomia {
             Psi = new ည(Mu->Mu->Theta->Mu, Mu->Upsilon);
             Eta = new ည(Mu->Psi->Theta->Mu, Mu->Upsilon);
             Upsilon = Psi->Pi();
+            Theta = Eta->Pi();
         }
 
         vector<Faung*> Affinities() {
@@ -28,6 +30,7 @@ namespace Dysnomia {
             vector<Faung*> Omicron = Eta->Affinities();
             Alpha.insert(Alpha.end(), Omicron.begin(), Omicron.end());
             Alpha.push_back(Upsilon);
+            Alpha.push_back(Theta);
             return Alpha;
         }
 
@@ -36,6 +39,7 @@ namespace Dysnomia {
             if (Psi->Gamma == 1) delete Psi; else Psi->Gamma--;
             if (Eta->Gamma == 1) delete Eta; else Eta->Gamma--;
             if (Upsilon->Rod->Gamma <= 1 || Upsilon->Cone->Gamma <= 1) delete Upsilon; else { Upsilon->Rod->Gamma--; Upsilon->Cone->Gamma--; }
+            if (Theta->Rod->Gamma <= 1 || Theta->Cone->Gamma <= 1) delete Theta; else { Theta->Rod->Gamma--; Theta->Cone->Gamma--; }
         }
     };
 }
