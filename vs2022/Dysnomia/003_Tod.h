a8dc0d0a14e71f1e33f111abd2ee97dfd09471e2
maria <maria@twee.se>
2023-08-04 11:49:47 -0700

    Playing Tod Stricture 212

--- vs2022/Dysnomia/003_Tod.h
+++ vs2022/Dysnomia/003_Tod.h
@@ -19,7 +19,7 @@ namespace Dysnomia {
             Psi = new ညြ(true);
             Psi->Theta->Rho->Fuse(Mu->Theta->Mu->Cone->Base, Mu->Theta->Psi->Cone->Element, Mu->Mu->Psi->Cone->Manifold);
             Psi->Theta->Rho->Tune();
-            Mu->Theta->Rho->Fuse(Psi->Theta->Mu->Rod->Signal, Psi->Theta->Psi->Rod->Channel, Psi->Mu->Psi->Rod->Identity);
+            Mu->Theta->Rho->Fuse(Psi->Theta->Rho->Signal, Psi->Theta->Rho->Channel, Psi->Mu->Rho->Identity);
             Mu->Theta->Rho->Tune();
             Upsilon = Mu->Pi();
             Tau = Psi->Pi();
