1a4223b8aa11c3f0f82e8955955d71164972b156
maria <maria@twee.se>
2023-08-04 17:59:59 -0700

    Persistent 锚 & Mutex Instead Of Atomics

--- vs2022/Dysnomia/Dysnomia.cpp
+++ vs2022/Dysnomia/Dysnomia.cpp
@@ -3,6 +3,7 @@
 #include <time.h>
 #include <iostream>
 #include <thread>
+#include <mutex>
 #include "006_锚.h"
 
 using namespace Dysnomia;
@@ -10,7 +11,9 @@ using namespace std;
 
 const int nthreads = 11;
 std::atomic<int> counter;
-std::atomic<Int64> Phi;
+mutex Mu_Mutex;
+锚* Mu;
+Int64 Phi;
 
 void Tau();
 
@@ -24,6 +27,8 @@ int main()
 
     counter = 0;
     Phi = 0;
+
+    Mu = new 锚();
     
     for (int i = 0; i < threads.size(); i++) {
         threads[i] = thread(Tau);
@@ -32,10 +37,10 @@ int main()
     for (thread& th : threads)
         th.join();
 
+    if (Mu->Gamma == 1) delete Mu; else Mu->Gamma--;
 }
 
 void Tau() {
-    锚* Mu;
     Fa* Rho;
     Fa* Psi;
     //Faung* Nu;
@@ -43,16 +48,16 @@ void Tau() {
     int local_count;
 
     for (;;) {
-        Mu = new 锚();
+        Mu_Mutex.lock();
         Rho = Mu->Pi(Phi);
         Psi = Mu->Pi(Rho->Eta);
         Phi = Psi->Eta;
+        Mu_Mutex.unlock();
 
-        if (Mu->Gamma == 1) delete Mu; else Mu->Gamma--;
         if (Rho->Gamma == 1) delete Rho; else Rho->Gamma--;
         if (Psi->Gamma == 1) delete Psi; else Psi->Gamma--;
         //if (Nu->Rod->Gamma <= 1 || Nu->Cone->Gamma <= 1) delete Nu; else { Nu->Rod->Gamma--; Nu->Cone->Gamma--; }
-        std::this_thread::sleep_for(chrono::nanoseconds(rand() % 1000));
+        //std::this_thread::sleep_for(chrono::nanoseconds(rand() % 100));
 
         local_count = ++counter;
         if (local_count % 10000 == 0) cout << "锚";
